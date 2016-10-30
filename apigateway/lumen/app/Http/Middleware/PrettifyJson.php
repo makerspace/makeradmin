15e942dea44740e3b44b396fe14d24a15b142b8f
Christian Antila <tagged-github.com@chille.se>
2016-10-30 13:05:50 +0100

    Improved JSON prettifier

--- apigateway/lumen/app/Http/Middleware/PrettifyJson.php
+++ apigateway/lumen/app/Http/Middleware/PrettifyJson.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
+use \Illuminate\Http\Response;
 
 use Closure;
 
@@ -16,42 +17,43 @@ class PrettifyJson
 	public function handle($request, Closure $next)
 	{
 		$response = $next($request);
+		$status_code = $response->getStatusCode();
 
 		// Prettify JSON
 		if($response instanceof \Illuminate\Http\JsonResponse)
 		{
 			// Use our own fancy class that prints it pretty!
-			return new Meep($response->getData());
+			return new Response(
+				$this->_formatJson($response->getData()),
+				$status_code
+			);
 		}
 		// The Passport stuff seems to return the JSON data as a normal Response, and not an JsonResponse
-		else if(($json = json_decode($response->getContent())) !== false)
+		else if(($json = json_decode($response->getContent())) !== null)
 		{
 			// Use our own fancy class that prints it pretty!
-			return new Meep($json);
+			return new Response(
+				$this->_formatJson($json),
+				$status_code
+			);
 		}
 
 		if(!method_exists($response, "render"))
 		{
-			return $response;
+			return new Response(
+				$this->_formatJson(["message" => (string)$response->original]),
+				$status_code
+			);
 		}
 
 		$content = $response->render()."\n";
 
 		return $content;
 	}
-}
-use Illuminate\Contracts\Support\Jsonable;
-class Meep
-{
-	protected $data;
 
-	function __construct($data)
+	public function _formatJson($json)
 	{
-		$this->data = $data;
-	}
-
-	function __toString()
-	{
-		return json_encode($this->data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)."\n";
+		$str = json_encode($json, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+		return "{$str}\n";
 	}
 }
