61edb36f817dc7136c6d9b6c1f90662d0953d522
Christian Antila <tagged-github.com@chille.se>
2016-12-03 03:26:42 +0100

    Restructured routes

--- economy/lumen/routes/web.php
+++ economy/lumen/routes/web.php
@@ -12,43 +12,53 @@
 */
 
 // Economy
-$app->group(array("prefix" => "economy/{accountingperiod}"), function() use ($app)
+$app->group(array("namespace" => "App\Http\Controllers", "prefix" => "economy"), function() use ($app)
 {
-	// Files / vouchers
-	$app->   get("file/{external_id}/{filename}", "Economy@file"); // Get collection
+	// Accounting periods
+	$app->   get("accountingperiod",      "AccountingPeriod@list");   // Get collection
+	$app->  post("accountingperiod",      "AccountingPeriod@create"); // Model: Create
+	$app->   get("accountingperiod/{id}", "AccountingPeriod@read");   // Model: Read
+	$app->   put("accountingperiod/{id}", "AccountingPeriod@update"); // Model: Update
+	$app->delete("accountingperiod/{id}", "AccountingPeriod@delete"); // Model: Delete
+});
 
-	// Transactions
-	$app->   get("transaction",      "Transactions@list");     // Get collection
-	$app->  post("transaction",      "Transactions@create");   // Model: Create
-	$app->   get("transaction/{id}", "Transactions@read");     // Model: Read
-	$app->   put("transaction/{id}", "Transactions@update");   // Model: Update
-	$app->delete("transaction/{id}", "Transactions@delete");   // Model: Delete
+$app->group(array("namespace" => "App\Http\Controllers", "prefix" => "economy/{accountingperiod}"), function() use ($app)
+{
+	// Accounts
+	$app->   get("account",          "Account@list");        // Get collection
+	$app->  post("account",          "Account@create");      // Model: Create
+	$app->   get("account/{id}",     "Account@read");        // Model: Read
+	$app->   put("account/{id}",     "Account@update");      // Model: Update
+	$app->delete("account/{id}",     "Account@delete");      // Model: Delete
 
 	// Instructions
-	$app->   get("instruction",      "Instruction@list");      // Get collection
-	$app->  post("instruction",      "Instruction@create");    // Model: Create
-	$app->   get("instruction/{id}", "Instruction@read");      // Model: Read
-	$app->   put("instruction/{id}", "Instruction@update");    // Model: Update
-	$app->delete("instruction/{id}", "Instruction@delete");    // Model: Delete
+	$app->   get("instruction",      "Instruction@list");    // Get collection
+	$app->  post("instruction",      "Instruction@create");  // Model: Create
+	$app->   get("instruction/{id}", "Instruction@read");    // Model: Read
+	$app->   put("instruction/{id}", "Instruction@update");  // Model: Update
+	$app->delete("instruction/{id}", "Instruction@delete");  // Model: Delete
+
+	// Transactions
+	$app->   get("transaction",      "Transactions@list");   // Get collection
+	$app->  post("transaction",      "Transactions@create"); // Model: Create
+	$app->   get("transaction/{id}", "Transactions@read");   // Model: Read
+	$app->   put("transaction/{id}", "Transactions@update"); // Model: Update
+	$app->delete("transaction/{id}", "Transactions@delete"); // Model: Delete
+
+	// Cost centers
+	$app->   get("costcenter",       "Costcenter@list");     // Get collection
+	$app->  post("costcenter",       "Costcenter@create");   // Model: Create
+	$app->   get("costcenter/{id}",  "Costcenter@read");     // Model: Read
+	$app->   put("costcenter/{id}",  "Costcenter@update");   // Model: Update
+	$app->delete("costcenter/{id}",  "Costcenter@delete");   // Model: Delete
 
 	// Master ledger
 	$app->   get("masterledger",     "Account@masterledger");  // Get collection
 
-	// Accounts
-	$app->   get("account",          "App\Http\Controllers\Account@list");          // Get collection
-	$app->  post("account",          "Account@create");        // Model: Create
-	$app->   get("account/{id}",     "Account@read");          // Model: Read
-	$app->   put("account/{id}",     "Account@update");        // Model: Update
-	$app->delete("account/{id}",     "Account@delete");        // Model: Delete
-
-	// Cost centers
-	$app->   get("costcenter",       "Costcenter@list");       // Get collection
-	$app->  post("costcenter",       "Costcenter@create");     // Model: Create
-	$app->   get("costcenter/{id}",  "Costcenter@read");       // Model: Read
-	$app->   put("costcenter/{id}",  "Costcenter@update");     // Model: Update
-	$app->delete("costcenter/{id}",  "Costcenter@delete");     // Model: Delete
-
 	// Reports
 	$app->   get("valuationsheet",   "Report@valuationSheet"); // Get collection
-	$app->   get("resultreport",     'App\Http\Controllers\Report@resultReport');   // Get collection
+	$app->   get("resultreport",     "Report@resultReport");   // Get collection
+
+	// Files / vouchers
+	$app->   get("file/{external_id}/{filename}", "Economy@file"); // Get collection
 });
