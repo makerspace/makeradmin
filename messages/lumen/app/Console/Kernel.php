b3fe4aa174e9c7cccf0a8d809c1b99e44eec0cb8
Christian Antila <tagged-github.com@chille.se>
2016-12-05 14:25:00 +0100

    Service register/unregister stuff

--- messages/lumen/app/Console/Kernel.php
+++ messages/lumen/app/Console/Kernel.php
@@ -7,23 +7,24 @@ use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        //
-    ];
+	/**
+	 * The Artisan commands provided by your application.
+	 *
+	 * @var array
+	 */
+	protected $commands = [
+		Commands\ServiceRegister::class,
+		Commands\ServiceUnregister::class,
+	];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule)
-    {
-        //
-    }
+	/**
+	 * Define the application's command schedule.
+	 *
+	 * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+	 * @return void
+	 */
+	protected function schedule(Schedule $schedule)
+	{
+		//
+	}
 }
---
+++ messages/lumen/config/service.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'name'    => 'Messages service',
+	'version' => '1.0',
+	'url'     => 'messages',
+	'gateway' => getenv('APIGATEWAY'),
+];
