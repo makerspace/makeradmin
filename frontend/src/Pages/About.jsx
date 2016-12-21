2a567d3e6a10d48e25f1d12bb41c9ce230ef455e
Christian Antila <tagged-github.com@chille.se>
2016-12-21 16:39:11 +0100

    About page with information about software version, dependencies, etc

---
+++ frontend/src/Pages/About.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import config from '../config'
+
+// Get versions of dependencies
+const ReactVersion    = require("json-loader!../../node_modules/react/package.json").version;
+const JqueryVersion = require("json-loader!../../node_modules/jquery/package.json").version;
+const UikitVersion = require("json-loader!../../node_modules/uikit/package.json").version;
+const BackboneVersion = require("json-loader!../../node_modules/backbone/package.json").version;
+const BackboneReactVersion = require("json-loader!../../node_modules/backbone-react-component//package.json").version;
+const BackbonePaginatorVersion = require("json-loader!../../node_modules/backbone.paginator//package.json").version;
+const ReactRouterVersion = require("json-loader!../../node_modules/react-router/package.json").version;
+const ReactSelectVersion = require("json-loader!../../node_modules/react-select/package.json").version;
+const ReactDomVersion = require("json-loader!../../node_modules/react-dom/package.json").version;
+
+module.exports = React.createClass({
+	render: function ()
+	{
+		return (
+			<div>
+				<h2>Build</h2>
+				<dl>
+					<dt>Build date:</dt>
+					<dd>{__BUILD_DATE__}</dd>
+
+					<dt>Build hash:</dt>
+					<dd>{__COMMIT_HASH__}</dd>
+				</dl>
+
+				<h2>API</h2>
+				<dl>
+					<dt>API Version:</dt>
+					<dd>{config.apiVersion}</dd>
+
+					<dt>API endpoint:</dt>
+					<dd>{config.apiBasePath}</dd>
+				</dl>
+
+				<h2>Included dependencies:</h2>
+				<dl>
+					<dt>React version:</dt>
+					<dd>{ReactVersion}</dd>
+
+					<dt>jQuery version:</dt>
+					<dd>{JqueryVersion}</dd>
+
+					<dt>uikit version:</dt>
+					<dd>{UikitVersion}</dd>
+
+					<dt>Backbone version:</dt>
+					<dd>{BackboneVersion}</dd>
+
+					<dt>backbone-react-component version:</dt>
+					<dd>{BackboneReactVersion}</dd>
+
+					<dt>backbone.paginator version:</dt>
+					<dd>{BackbonePaginatorVersion}</dd>
+
+					<dt>react-router version:</dt>
+					<dd>{ReactRouterVersion}</dd>
+
+					<dt>react-select version:</dt>
+					<dd>{ReactSelectVersion}</dd>
+
+					<dt>react-dom version:</dt>
+					<dd>{ReactDomVersion}</dd>
+				</dl>
+
+				<h2>License</h2>
+				<p>GPL version 3</p>
+			</div>
+		);
+	}
+});
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,17 +1,35 @@
+var webpack = require('webpack');
+
+// Get git info from command line
+var commitHash = require("child_process")
+	.execSync("git rev-parse --short HEAD")
+	.toString();
+
+// Get the build date
+var options = {
+	year: 'numeric', month: 'numeric', day: 'numeric',
+	hour: 'numeric', minute: 'numeric', second: 'numeric',
+	hour12: false
+};
+var buildDate = new Intl.DateTimeFormat('sv-SE', options).format(new Date());
+
 module.exports = {
 	context: __dirname + "/src",
 	entry: "./app.jsx",
 
+	// Compile into a js.app
 	output:
 	{
 		filename: "app.js",
 		path: __dirname + "/dist/js",
 	},
 
+	// Include *.js and *.jsx files
 	resolve: {
 		extensions: ["", ".js", ".jsx"]
 	},
 
+	// Preprocess *.jsx files
 	module: {
 		loaders:
 		[
@@ -25,4 +43,12 @@ module.exports = {
 			},
 		],
 	},
+
+	// Include build information (build date, git hash)
+	plugins: [
+		new webpack.DefinePlugin({
+			__COMMIT_HASH__: JSON.stringify(commitHash),
+			__BUILD_DATE__: JSON.stringify(buildDate),
+		})
+	]
 }
