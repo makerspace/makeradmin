2bab512922502183335e0aa5f11e85bbb019515e
Christian Antila <tagged-github.com@chille.se>
2016-12-31 06:03:14 +0100

    Better batch processing of messages

--- messages/lumen/app/Http/Controllers/Message.php
+++ messages/lumen/app/Http/Controllers/Message.php
@@ -30,7 +30,8 @@ class Message extends Controller
 	 */
 	function create(Request $request)
 	{
-		$json = $request->json()->all();
+		// TODO: Get the chunk size from configuration
+		$chunkSize = 2500;
 
 		// TODO: Validate
 		//   $json["recipients"][$i]["type"] == member|group
@@ -38,38 +39,25 @@ class Message extends Controller
 		//   $json["message_type"]
 		//   $json["subject"] null if sms or not null if e-mail
 		//   $json["body"]
+		$json = $request->json()->all();
 
 		// Load all members / groups from the API and put together in an distinct list (no duplicates)
 		$recipientList = [];
+		$memberIds = [];
 		foreach($json["recipients"] as $recipient)
 		{
 			// Load the recipient
 			if($recipient["type"] == "member")
 			{
-				// If the recipient is a member, load member from API
-				$ch = new CurlBrowser;
-				// TODO: Gateway from config
-				$result = $ch->call("GET", "http://makeradmin-apigateway/membership/member/{$recipient["id"]}");
-
-				// Send an error message if the API request was unsuccessful
-				if($ch->getStatusCode() != 200)
-				{
-					return Response()->json([
-						"status" => "error",
-						"message" => "An unexpected error occured while trying to load data from the membership module",
-					], 502);
-				}
-
-				// Add the member to the recipient list
-				$member = $ch->getJson();
-				$recipientList[$member->member_id] = $member;
+				// Save them in an array and batch process them
+				$memberIds[] = $recipient["id"];
 			}
 			else if($recipient["type"] == "group")
 			{
 				// If the recipient is a group, get group list from API
 				$ch = new CurlBrowser;
 				// TODO: Gateway from config
-				$result = $ch->call("GET", "http://makeradmin-apigateway/membership/group/{$recipient["id"]}/members");
+				$result = $ch->call("GET", "http://makeradmin-apigateway/membership/group/{$recipient["id"]}/members?per_page={$chunkSize}");
 
 				// Send an error message if the API request was unsuccessful
 				if($ch->getStatusCode() != 200)
@@ -81,12 +69,39 @@ class Message extends Controller
 				}
 
 				// Add the members to the recipient list
-				$result = $ch->getJson();
-				foreach($result->data as $member)
+				foreach($ch->getJson()->data as $member)
 				{
 					$recipientList[$member->member_id] = $member;
 				}
 			}
+		}
+
+		// Batch process members
+		foreach(array_chunk($memberIds, $chunkSize) as $chunk)
+		{
+			$str_ids = implode(",", $chunk);
+//			foreach($chunk as $member_id)
+//			{
+			// If the recipient is a member, load member from API
+			$ch = new CurlBrowser;
+			// TODO: Gateway from config
+			$result = $ch->call("GET", "http://makeradmin-apigateway/membership/member?member_ids={$str_ids}&per_page={$chunkSize}");
+
+			// Send an error message if the API request was unsuccessful
+			if($ch->getStatusCode() != 200)
+			{
+				return Response()->json([
+					"status" => "error",
+					"message" => "An unexpected error occured while trying to load data from the membership module",
+				], 502);
+			}
+
+			// Add the member to the recipient list
+			foreach($ch->getJson()->data as $member)
+			{
+				$recipientList[$member->member_id] = $member;
+			}
+//			}
 		}
 
 		// Create new mail
@@ -128,13 +143,13 @@ class Message extends Controller
 
 			// Create new recipient
 			$entity = new RecipientModel;
-			$entity->message_id  = $message->entity_id;
-			$entity->subject     = $subject;
-			$entity->body        = $body;
-			$entity->member_id   = $member->member_id;
-			$entity->recipient   = $recipient;
-			$entity->date_sent   = null;
-			$entity->status      = "queued";
+			$entity->message_id = $message->entity_id;
+			$entity->subject    = $subject;
+			$entity->body       = $body;
+			$entity->member_id  = $member->member_id;
+			$entity->recipient  = $recipient;
+			$entity->date_sent  = null;
+			$entity->status     = "queued";
 
 			// Validate input
 			$entity->validate();
@@ -157,7 +172,7 @@ class Message extends Controller
 	{
 		// Load the product
 		$message = MessageModel::load([
-			["message_id", "=", $message_id]
+			"message_id" => ["=", $message_id]
 		]);
 
 		// Generate an error if there is no such product
