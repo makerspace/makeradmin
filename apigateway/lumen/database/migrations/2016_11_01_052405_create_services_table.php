6d197378d610134ad3f24a3ad3d15d45899cb98e
Christian Antila <tagged-github.com@chille.se>
2017-01-06 13:47:05 +0100

    Updated common files

--- apigateway/lumen/database/migrations/2016_11_01_052405_create_services_table.php
+++ apigateway/lumen/database/migrations/2016_11_01_052405_create_services_table.php
@@ -20,9 +20,10 @@ class CreateServicesTable extends Migration
 			$table->string("url");
 			$table->string("endpoint");
 			$table->string("version", 12);
+
 			$table->dateTimeTz("created_at")->default(DB::raw("CURRENT_TIMESTAMP"));
-			$table->dateTimeTz("updated_at")->default(DB::raw("CURRENT_TIMESTAMP"));
-			$table->softDeletes();
+			$table->dateTimeTz("updated_at")->nullable();
+			$table->dateTimeTz("deleted_at")->nullable();
 
 			$table->index("url");
 			$table->index("version");
