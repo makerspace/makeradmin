dd1a9b34c4db8b677ead77d92d8d21ec3fb23261
Christian Antila <tagged-github.com@chille.se>
2017-01-07 21:48:36 +0100

    Change password CLI

--- membership/lumen/app/Console/Kernel.php
+++ membership/lumen/app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
 	 */
 	protected $commands = [
 		Commands\CreateUser::class,
+		Commands\ChangePassword::class,
 		Commands\ServiceRegister::class,
 		Commands\ServiceUnregister::class,
 	];
