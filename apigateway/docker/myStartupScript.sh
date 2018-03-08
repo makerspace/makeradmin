2a78c9836eb46f2d724b918aaf84046133cbc191
Aron Granberg <aron.granberg@gmail.com>
2018-03-08 09:14:07 -0500

    Updated scripts to work with the latest hhvm docker image

--- apigateway/docker/myStartupScript.sh
+++ apigateway/docker/myStartupScript.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+mkdir -p /var/www/html/storage
+mkdir -p /var/www/html/bootstrap/cache
+
 # Make sure the webserver have write permissions
 chown -R www-data:www-data /var/www/html/storage/
 chown -R www-data:www-data /var/www/html/bootstrap/cache/
 
 # Start HHVM
-/usr/bin/hhvm -m server -c /etc/hhvm/server.ini -c /etc/hhvm/site.ini
+/usr/bin/hhvm -vServer.AllowRunAsRoot=1 -m server -c /etc/hhvm/server.ini -c /etc/hhvm/site.ini
