042b496c9b1ed976a98794e3f8e7f27482ebc535
Johan Kristensen <johankristensen@gmail.com>
2018-03-21 17:52:50 +0100

    Make migrate.sh script independent

--- migrate.sh
+++ migrate.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-find -path "*/docker/artisan" | while read f; do
-	echo $f
-	source $(echo "$f" | sed -e "s/artisan/config/")
-	docker exec $CONTAINER_NAME /usr/bin/php /var/www/html/artisan --force migrate
-done
+DB_RUNNING=`docker-compose ps -q db2`
+[ -z "$DB_RUNNING" ] && docker-compose up -d db2
+docker-compose exec db2 bash -c "while ! mysqladmin ping --silent; do echo 'waiting for database' && sleep 1; done"
+docker-compose run --rm --no-deps api-gateway /usr/bin/php /var/www/html/artisan --force migrate
+docker-compose run --rm --no-deps membership /usr/bin/php /var/www/html/artisan --force migrate
+docker-compose run --rm --no-deps messages /usr/bin/php /var/www/html/artisan --force migrate
+docker-compose run --rm --no-deps sales /usr/bin/php /var/www/html/artisan --force migrate
+docker-compose run --rm --no-deps economy /usr/bin/php /var/www/html/artisan --force migrate
+[ -z "$DB_RUNNING" ] && docker-compose down
--- readme.md
+++ readme.md
@@ -5,11 +5,18 @@ This is work in progress and may do absolutely anything or nothing at all
 -------------------------------------------------------------------------
 
 ~~~
+# Install dependencies
+sudo apt-get install docker.io docker-compose
+
 # clone this repository...
 cd repository
 make update
+#Build docker images
+make build
+# To create (or migrate) all database tables, (run once)
+make migrate
+
+#Start MakerAdmin
 make run
 
-# To create (or migrate) all database tables, run this while everything is running
-make migrate
 ~~~
