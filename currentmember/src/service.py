a4e47a808bb32627e52fdfa24794f165ae206c60
Aron Granberg <aron.granberg@gmail.com>
2018-03-21 11:44:26 -0400

    Initial commit

---
+++ currentmember/src/service.py
@@ -0,0 +1,155 @@
+from flask import abort, jsonify
+import pymysql
+import json
+import sys
+import os
+import requests
+import socket
+import signal
+
+class DB:
+    def __init__(self, host, name, user, password):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.table_prefix = ""
+        self.connection = None
+
+    def connect(self):
+        self.connection = pymysql.connect(host=self.host.split(":")[0], port=int(self.host.split(":")[1]), db=self.name, user=self.user, password=self.password)
+
+    def cursor(self):
+        return self.connection.cursor()
+
+class APIGateway:
+    def __init__(self, host, key):
+        self.host = host
+        self.key = key
+
+    def get(self, path, payload=None):
+        headers = { "Authorization": "Bearer " + self.key }
+        return requests.get('http://' + self.host + "/" + path, params=payload, headers=headers)
+
+    def post(self, path, payload):
+        headers = { "Authorization": "Bearer " + self.key }
+        return requests.post('http://' + self.host + "/" + path, json=payload, headers=headers)
+
+    def put(self, path, payload):
+        headers = { "Authorization": "Bearer " + self.key }
+        return requests.put('http://' + self.host + "/" + path, json=payload, headers=headers)
+
+class Service:
+    def __init__(self, name, url, port, version, db, gateway, debug):
+        self.name = name
+        self.url = url
+        self.port = port
+        self.version = version
+        self.db = db
+        self.debug = debug
+        self.gateway = gateway
+
+    def full_path(self, path):
+        return "/" + self.url + "/" + path
+
+    def register(self):
+        payload = {
+            "name": self.name,
+            "url": self.url,
+            "endpoint": "http://" + socket.gethostname() + ":" + str(self.port) + "/",
+            "version": self.version
+        }
+        r = self.gateway.post("service/register", payload)
+        if not r.ok:
+            raise Exception("Failed to register service: " + r.text)
+
+    def unregister(self):
+        payload = {
+            "url": self.url,
+            "version": self.version
+        }
+        r = self.gateway.post("service/unregister", payload)
+        if not r.ok:
+            raise Exception("Failed to register service: " + r.text)
+
+    def wrap_error_codes(self, app):
+        # Pretty ugly way to wrap all abort(code, message) calls so that they return proper json reponses
+        def create_wrapper(status_code):
+            @app.errorhandler(status_code)
+            def custom_error(error):
+                response = jsonify({'status': error.description})
+                response.status_code = status_code
+                return response
+        for i in range(400, 500):
+            try:
+                create_wrapper(i)
+            except:
+                pass
+
+    def serve_indefinitely(self, app):
+        capture_signals()
+        self.wrap_error_codes(app)
+        try:
+            app.run(host='0.0.0.0', debug=self.debug, port=self.port, use_reloader=False)
+        finally:
+            eprint("Closing database connection")
+            self.db.connection.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.unregister()
+
+def assert_get(data, key):
+    if key not in data:
+        abort(400, "Missing required parameter " + key)
+
+    return data[key]
+
+def _read_config():
+    try:
+        db = DB(
+            host=os.environ["MYSQL_HOST"],
+            name=os.environ["MYSQL_DB"],
+            user=os.environ["MYSQL_USER"],
+            password=os.environ["MYSQL_PASS"],
+        )
+
+        gateway = APIGateway(os.environ["APIGATEWAY"], os.environ["BEARER"])
+
+        debugStr = os.environ["APP_DEBUG"].lower()
+        if debugStr == "true" or debugStr == "false":
+            debug = debugStr == "true"
+        else:
+            raise Exception("APP_DEBUG environment variable must be either 'true' or 'false'. Found '{}'".format(debugStr))
+    except Exception as e:
+        eprint("Missing one or more configuration environment variables")
+        sys.exit(1)
+    return db, gateway, debug
+
+def eprint(s, **kwargs):
+    ''' Print to stderr and flush the output stream '''
+    print(s, **kwargs, file=sys.stderr)
+    sys.stderr.flush()
+
+def capture_signals():
+    def signal_handler(signal, frame):
+        raise KeyboardInterrupt()
+
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+    signal.signal(signal.SIGHUP, signal_handler)
+
+def create(name, url, port, version):
+    db, gateway, debug = _read_config()
+    db.table_prefix = url + "_"
+    service = Service(name, url, port, version, db, gateway, debug)
+
+    service.unregister()
+    eprint("Registering service...")
+    service.register()
+
+    eprint("Connecting to database...")
+    db.connect()
+    return service
---
+++ currentmember/src/user.py
@@ -0,0 +1,90 @@
+from flask import Flask, request, abort
+import json
+
+import service
+from service import eprint, assert_get
+
+with service.create(name="Makerspace Member Login", url="member", port=80, version="1.0") as instance:
+    app = Flask(__name__)
+
+    # Grab the database so that we can use it inside requests
+    db = instance.db
+
+    @app.route(instance.full_path("send_access_token"), methods=["POST"])
+    def send_access_token():
+        eprint(request.data)
+        data = request.get_json()
+        if data is None:
+            abort(400, "missing json")
+
+        # The external search is maybe a bit too permissive, for example just entering a single digit that is included in e.g the zip-code includes that user in the search results
+        use_external_search = False
+        user_tag = assert_get(data, "user_tag")
+        if use_external_search:
+            response = instance.gateway.get("membership/member?search=%s" % user_tag)
+            if response.status_code != 200:
+                return app.response_class(
+                    response=response.text,
+                    status=response.status_code
+                )
+
+            matching = assert_get(json.loads(response.text), "data")
+            if len(matching) == 0:
+                abort(400, "not found")
+            if len(matching) > 1:
+                abort(400, "ambigious")
+            user_id = matching[0]["member_id"]
+        else:
+            with db.cursor() as cur:
+                if user_tag.isdigit():
+                    cur.execute("SELECT member_id FROM membership_members WHERE member_number=%s", (user_tag,))
+                else:
+                    cur.execute("SELECT member_id FROM membership_members WHERE email=%s", (user_tag,))
+                matching = cur.fetchall()
+                if len(matching) == 0:
+                    abort(400, "not found")
+                if len(matching) > 1:
+                    abort(400, "ambigious")
+                user_id = matching[0][0]
+
+        # This should be sent via an email, but lets just return it for now
+        return instance.gateway.post("oauth/force_token", {"user_id": user_id}).text
+
+    @app.route(instance.full_path("current"), methods=["GET", "POST"])
+    def current_member():
+        user_id = assert_get(request.headers, "X-User-Id")
+
+        if request.method == "GET":
+            return instance.gateway.get("membership/member/%s" % user_id).text
+        else:
+            return instance.gateway.put("membership/member/%s" % user_id, request.get_json()).text
+
+    @app.route(instance.full_path("current/keys"), methods=["GET"])
+    def key_info():
+        ''' List of keys that the current logged in member has '''
+        user_id = assert_get(request.headers, "X-User-Id")
+        return instance.gateway.get("related?param=/membership/member/%s&matchUrl=/keys/(.*)&from=keys&page=1&sort_by=&sort_order=asc&per_page=10000" % user_id).text
+
+    # def initialize_db():
+    #     with db.cursor() as cur:
+    #         cur.execute("CREATE TABLE IF NOT EXISTS member_access_tokens (user_id INT(11) NOT NULL, access_token VARCHAR(48) NOT NULL, expires datetime NOT NULL);")
+    #     db.connection.commit()
+
+    # eprint("Sending email...")
+    # eprint(instance.gateway.get("messages").text)
+
+    # r = instance.gateway.post("messages", {
+    #     "recipients": [
+    #         {
+    #             "type": "member",
+    #             "id": 1
+    #         },
+    #     ],
+    #     "message_type": "email",
+    #     "subject": "Hello This a test from MakerAdmin",
+    #     "body": "Trying to send an email from MakerAdmin"
+    # })
+    # eprint(r.text)
+
+    # initialize_db()
+    instance.serve_indefinitely(app)
