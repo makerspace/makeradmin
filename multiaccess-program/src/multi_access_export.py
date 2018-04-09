e0707e7de38975dcdde0592745508084782340e8
Anders Roos <anders.roos@gmail.com>
2018-04-09 22:58:05 +0200

    Successful building windows executable that connects to the MultiAccess db.

--- multiaccess-program/src/multi_access_export.py
+++ multiaccess-program/src/multi_access_export.py
@@ -2,7 +2,12 @@
 
 import os
 import sys
+import pyodbc
 from logging import basicConfig, INFO, getLogger
+
+
+windows = os.name == 'nt'
+
 
 basicConfig(format='%(asctime)s %(levelname)s [%(process)d/%(threadName)s %(pathname)s:%(lineno)d]: %(message)s',
             stream=sys.stderr, level=INFO)
@@ -13,19 +18,29 @@ logger = getLogger("makeradmin")
 
 def main():
 
-    logger.info("I am MultiAccessSync, hello!")
+    connection = pyodbc.connect(Trusted_Connection='yes',
+                                driver='{SQL Server}',
+                                server='(local)\SQLEXPRESS',
+                                database='MultiAccess')
     
-    raise Exception("Hej")
+    cursor = connection.cursor()
+    cursor.execute("select * from Users")
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.tables()
+    for row in cursor.fetchall():
+        print(row.table_name)
     
     
 if __name__ == '__main__':
 
     try:
         main()
-        if os.name == 'nt':
+        if windows:
             input("Export finished, enter to close: ")
     except Exception as e:
-        if os.name == 'nt':
+        if windows:
             logger.exception(e)
             input("Program failed, enter to close: ")
         else:
