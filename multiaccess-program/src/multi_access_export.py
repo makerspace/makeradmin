9e73de50350dd84443ff3c75288fde877e1fe10d
Anders Roos <anders.roos@gmail.com>
2018-04-10 22:44:59 +0200

    SQL Alchemy db connection string to mssql.

--- multiaccess-program/src/multi_access_export.py
+++ multiaccess-program/src/multi_access_export.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-
+import argparse
 import os
 import sys
 import pyodbc
 from logging import basicConfig, INFO, getLogger
 
+from sqlalchemy import create_engine, MetaData
 
 windows = os.name == 'nt'
 
@@ -18,19 +19,34 @@ logger = getLogger("makeradmin")
 
 def main():
 
-    connection = pyodbc.connect(Trusted_Connection='yes',
-                                driver='{SQL Server}',
-                                server='(local)\SQLEXPRESS',
-                                database='MultiAccess')
-    
-    cursor = connection.cursor()
-    cursor.execute("select * from Users")
-    for row in cursor.fetchall():
-        print(row)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--db", default='mssql://(local)\\SQLEXPRESS/MultiAccess?trusted_connection=yes&driver=SQL+Server',
+                        help="SQL Alchemy db engine spec.")
+                        
+    args = parser.parse_args()
 
-    cursor.tables()
-    for row in cursor.fetchall():
-        print(row.table_name)
+    logger.info(f"connecting to {args.db}")
+
+    engine = create_engine(args.db)
+    meta = MetaData()
+    meta.reflect(bind=engine)
+
+    for table in meta.tables:
+        print(table)
+    #
+    # connection = pyodbc.connect(Trusted_Connection='yes',
+    #                             driver='{SQL Server}',
+    #                             server='(local)\SQLEXPRESS',
+    #                             database='MultiAccess')
+    #
+    # cursor = connection.cursor()
+    # cursor.execute("select * from Users")
+    # for row in cursor.fetchall():
+    #     print(row)
+    #
+    # cursor.tables()
+    # for row in cursor.fetchall():
+    #     print(row.table_name)
     
     
 if __name__ == '__main__':
