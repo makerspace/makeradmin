a3f14031fb5af9d549415e221a5d7ed3dc9ed8cd
Anders Roos <anders.roos@gmail.com>
2018-04-10 22:49:19 +0200

    Removed windows exit, can fix later.

--- multiaccess-program/src/multi_access_export.py
+++ multiaccess-program/src/multi_access_export.py
@@ -2,13 +2,9 @@
 import argparse
 import os
 import sys
-import pyodbc
 from logging import basicConfig, INFO, getLogger
 
 from sqlalchemy import create_engine, MetaData
-
-windows = os.name == 'nt'
-
 
 basicConfig(format='%(asctime)s %(levelname)s [%(process)d/%(threadName)s %(pathname)s:%(lineno)d]: %(message)s',
             stream=sys.stderr, level=INFO)
@@ -20,7 +16,8 @@ logger = getLogger("makeradmin")
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--db", default='mssql://(local)\\SQLEXPRESS/MultiAccess?trusted_connection=yes&driver=SQL+Server',
+    parser.add_argument("-d", "--db",
+                        default='mssql://(local)\\SQLEXPRESS/MultiAccess?trusted_connection=yes&driver=SQL+Server',
                         help="SQL Alchemy db engine spec.")
                         
     args = parser.parse_args()
@@ -33,31 +30,9 @@ def main():
 
     for table in meta.tables:
         print(table)
-    #
-    # connection = pyodbc.connect(Trusted_Connection='yes',
-    #                             driver='{SQL Server}',
-    #                             server='(local)\SQLEXPRESS',
-    #                             database='MultiAccess')
-    #
-    # cursor = connection.cursor()
-    # cursor.execute("select * from Users")
-    # for row in cursor.fetchall():
-    #     print(row)
-    #
-    # cursor.tables()
-    # for row in cursor.fetchall():
-    #     print(row.table_name)
     
     
 if __name__ == '__main__':
 
-    try:
-        main()
-        if windows:
-            input("Export finished, enter to close: ")
-    except Exception as e:
-        if windows:
-            logger.exception(e)
-            input("Program failed, enter to close: ")
-        else:
-            raise
+    main()
+    
