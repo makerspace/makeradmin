5b3c77da6b06b2ffccb344808a60eb8659ba6488
Anders Roos <anders.roos@gmail.com>
2018-04-15 12:37:47 +0200

    Removed conversion to int since all fileds in the db are not ints.

--- multiaccess-program/src/export.py
+++ multiaccess-program/src/export.py
@@ -7,7 +7,7 @@ def export_to_json(db, customer_id=16):
     with db.connect() as conn:
         res = conn.execute(text("SELECT Name, Card, Stop FROM Users WHERE customerId = :customer_id"),
                            customer_id=customer_id)
-        return json.dumps([{'member_id': int(row['Name']), 'rfid_tag': int(row['Card']), 'end_timestamp': row['Stop']}
+        return json.dumps([{'member_id': row['Name'], 'rfid_tag': row['Card'], 'end_timestamp': str(row['Stop'])}
                            for row in res.fetchall()],
                           ensure_ascii=True,
                           indent=2)
