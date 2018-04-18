19a59110794e97bf489654bdb02aa64b5ab6fe03
Anders Roos <anders.roos@gmail.com>
2018-04-18 21:30:09 +0200

    Fixed pep8.

--- multiaccess-program/src/db_helper.py
+++ multiaccess-program/src/db_helper.py
@@ -1,6 +1,7 @@
 
 DB_NAME = 'mssql://(local)\\SQLEXPRESS/MultiAccess?trusted_connection=yes&driver=SQL+Server'
 
+
 def create_default_engine():
     import sqlalchemy
     db = sqlalchemy.create_engine(DB_NAME)
