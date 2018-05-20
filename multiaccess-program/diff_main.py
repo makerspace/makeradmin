aa6f1bc7ee36b1681210013608d0f0486ca8329f
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2018-05-20 20:28:39 +0200

    Changing default format to JSON
    - PKL format does not currently work, see GH issue #5

--- multiaccess-program/diff_main.py
+++ multiaccess-program/diff_main.py
@@ -52,7 +52,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", "--in-dir", default='.',
                         help="Dir where diff files are stored.")
-    parser.add_argument("-e", "--extension", default='pkl',
+    parser.add_argument("-e", "--extension", default='json',
                         help="Format pkl or json.")
     args = parser.parse_args()
 
