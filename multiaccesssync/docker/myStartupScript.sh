cb6bab234a8da11917e785089a991f30c7dd1e88
Aron Granberg <aron.granberg@gmail.com>
2018-05-26 20:11:05 +0200

    Ensure dependencies are ready

--- multiaccesssync/docker/myStartupScript.sh
+++ multiaccesssync/docker/myStartupScript.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+set -e
+# Wait for api-gateway to be ready
+/usr/local/myscripts/wait-for api-gateway:80
+
 
 # Not sure if these directories are needed, but let's create them anyway
 mkdir -p /var/www/html/storage/
