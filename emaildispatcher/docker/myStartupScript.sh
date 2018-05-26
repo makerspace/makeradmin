8a61a88cb68ab6d3bf03a8bdb85decdd5151b11a
Aron Granberg <aron.granberg@gmail.com>
2018-05-26 20:10:56 +0200

    Ensure dependencies are ready

--- emaildispatcher/docker/myStartupScript.sh
+++ emaildispatcher/docker/myStartupScript.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+set -e
+# Wait for database to be ready
+/usr/local/myscripts/wait-for "$MYSQL_HOST"
+# Wait for api-gateway to be ready
+/usr/local/myscripts/wait-for api-gateway:80
+
 exec /usr/bin/php -d hhvm.max_execution_time=0 /var/www/html/artisan service:send
