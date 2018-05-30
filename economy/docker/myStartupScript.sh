c2d094875f0f4c00ce741368e691b026730dbdfa
Johan K <johankristensen@gmail.com>
2018-05-31 00:44:11 +0200

    Upgrade lumen to v5.6 and replace HHVM by php v7.2

--- economy/docker/myStartupScript.sh
+++ economy/docker/myStartupScript.sh
@@ -1,35 +1,33 @@
 #!/bin/bash
 set -e
-# Wait for database to be ready
-/usr/local/myscripts/wait-for "$MYSQL_HOST"
+
 # Wait for api-gateway to be ready
 /usr/local/myscripts/wait-for api-gateway:80
 
 # Not sure if these directories are needed, but let's create them anyway
 mkdir -p /var/www/html/storage/
-mkdir -p /var/www/html/bootstrap/cache/
 
 # Make sure the webserver have write permissions
 chown -R www-data:www-data /var/www/html/storage/
-chown -R www-data:www-data /var/www/html/bootstrap/cache/
 
 shutdown() {
 	echo "Shutting down service"
-	/usr/bin/php -d hhvm.jit=0 /var/www/html/artisan service:unregister
-	kill -QUIT `cat /var/run/hhvm/pid`
+	php /var/www/html/artisan service:unregister
+	kill -QUIT `cat /var/run/nginx.pid`
+	kill -QUIT `cat /var/run/php/php7.2-fpm.pid`
 	exit 0
 }
 
 # Unregister the service when the script is shut down
 trap shutdown SIGHUP SIGINT SIGTERM
 
-# Register the service immediately
-# Disable the JIT because HHVM starts up really slowly with a JIT and this script is very short
-/usr/bin/php -d hhvm.jit=0 /var/www/html/artisan service:register
+# Start nginx
+echo "Starting server"
+php-fpm -R
+nginx
 
-# Start HHVM as a background process
-echo "Starting HHVM"
-/usr/bin/hhvm -vServer.AllowRunAsRoot=1 -m daemon -c /etc/hhvm/server.ini -c /etc/hhvm/site.ini &
+# Register the service immediately
+php /var/www/html/artisan service:register
 
 # Sleep forever (...or at least until the timestamp overflows :)
 # Note: We need to have the "& wait" to be able to trap signals while the sleep is running
