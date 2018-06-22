f6bc507cc8bef1dbef6904cac952a05bf80d0fb5
Aron Granberg <aron.granberg@gmail.com>
2018-06-22 14:40:31 +0200

    Simplify create_user script

--- create_user.py
+++ create_user.py
@@ -1,33 +1,8 @@
 #!/usr/bin/python3
-import requests
 import argparse
+import servicebase_python.service
 
-# Read the .env file
-env = {s[0]: (s[1] if len(s) > 1 else "") for s in (s.split("=") for s in open(".env").read().split('\n'))}
-
-# TODO: Need to read this port value from somewhere
-port = 8010
-
-
-class APIGateway:
-    def __init__(self, host, key):
-        self.host = host
-        self.key = key
-
-    def get(self, path, payload=None):
-        headers = {"Authorization": "Bearer " + self.key}
-        return requests.get('http://' + self.host + "/" + path, params=payload, headers=headers)
-
-    def post(self, path, payload):
-        headers = {"Authorization": "Bearer " + self.key}
-        return requests.post('http://' + self.host + "/" + path, json=payload, headers=headers)
-
-    def put(self, path, payload):
-        headers = {"Authorization": "Bearer " + self.key}
-        return requests.put('http://' + self.host + "/" + path, json=payload, headers=headers)
-
-
-gateway = APIGateway("localhost:" + str(port), env["API_BEARER"])
+gateway = servicebase_python.service.gateway_from_envfile(".env")
 
 parser = argparse.ArgumentParser(description='Create a new MakerAdmin user')
 parser.add_argument("--first-name", help="First name of the user", required=True)
