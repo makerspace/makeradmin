bf9ba8a38626e759b58b4f0f4654ab394da43e96
Aron Granberg <aron.granberg@gmail.com>
2018-07-19 22:05:28 +0200

    [webshop] Use multiple workers for the webshop frontend

--- webshop/src/run.sh
+++ webshop/src/run.sh
@@ -16,15 +16,30 @@ refresh
 if [ "$APP_DEBUG" == "true" ]; then
 	function watch_sass() {
 		echo "Starting sass watch process"
-		while inotifywait -qq -r -e modify,create,delete static; do
+		while inotifywait -qq -r -e modify,create,delete scss; do
 			echo "Updating stylesheets"
 			sleep 0.1
 			refresh
 		done
 	}
 	watch_sass&
+	function watch_ts() {
+		echo "Starting typescript watch process"
+		while inotifywait -qq -r -e modify,create,delete ts; do
+			echo "Updating typescript"
+			sleep 0.1
+			refresh_ts
+		done
+	}
+	watch_ts&
 fi
 
-# Exec replaces the shell with the service process which among other things allows signals to be sent directly to the service (e.g when docker wants to stop the container)
-exec gunicorn -b :80 frontend:instance.app&
-exec gunicorn -b :8000 backend:instance.app
+# Exec replaces the shell with the service process which among other things allows signals to be sent directly to the service (e.g when docker wants to stop the container).
+# Note: for some reason the workers seem to sometimes get blocked by persistent connections
+# (many browsers keep persistent connections to servers just in case they need them later).
+# The gthread worker class should help with this, but it doesn't always seem to work for some reason.
+# However running the servers behind nginx (and using the sync class) resolves all problems
+# as nginx handles all the persistent connections.
+# --log-level=DEBUG
+exec gunicorn --worker-class sync --workers=3 -b :80 frontend:instance.app&
+exec gunicorn --worker-class sync --workers=1 -b :8000 backend:instance.app
