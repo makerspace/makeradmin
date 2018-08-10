8c80c843446e30fc2f5e88dfb4767d154809c371
Anders Roos <anders.roos@gmail.com>
2018-08-10 12:33:57 +0200

    Upgraded to webpack 4.

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 // Get versions of dependencies
-const ReactVersion    = require("json-loader!../../node_modules/react/package.json").version;
-const JqueryVersion = require("json-loader!../../node_modules/jquery/package.json").version;
-const UikitVersion = require("json-loader!../../node_modules/uikit/package.json").version;
-const BackboneVersion = require("json-loader!../../node_modules/backbone/package.json").version;
-const BackboneReactVersion = require("json-loader!../../node_modules/backbone-react-component//package.json").version;
-const BackbonePaginatorVersion = require("json-loader!../../node_modules/backbone.paginator//package.json").version;
-const ReactRouterVersion = require("json-loader!../../node_modules/react-router/package.json").version;
-const ReactSelectVersion = require("json-loader!../../node_modules/react-select/package.json").version;
-const ReactDomVersion = require("json-loader!../../node_modules/react-dom/package.json").version;
+const ReactVersion    = require("../../node_modules/react/package.json").version;
+const JqueryVersion = require("../../node_modules/jquery/package.json").version;
+const UikitVersion = require("../../node_modules/uikit/package.json").version;
+const BackboneVersion = require("../../node_modules/backbone/package.json").version;
+const BackboneReactVersion = require("../../node_modules/backbone-react-component//package.json").version;
+const BackbonePaginatorVersion = require("../../node_modules/backbone.paginator//package.json").version;
+const ReactRouterVersion = require("../../node_modules/react-router/package.json").version;
+const ReactSelectVersion = require("../../node_modules/react-select/package.json").version;
+const ReactDomVersion = require("../../node_modules/react-dom/package.json").version;
 
 module.exports = React.createClass({
 	render: function ()
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,8 +1,14 @@
 var webpack = require('webpack');
+const path = require('path');
+
+var src = path.resolve(__dirname, "src")
+
+var mode = process.env.WEBPACK_SERVE || process.env.DEVELOPMENT ? 'development' : 'production'
+console.info("mode: " + mode);
 
 // Get git info from command line
 var commitHash = require("child_process")
-	.execSync("sh -c 'find /data/src/ -type f -exec md5sum {} \\; | sort -k 2 | md5sum'")
+	.execSync("sh -c 'find " + src + " -type f -exec md5sum {} \\; | sort -k 2 | md5sum'")
 	.toString();
 
 // Get the build date
@@ -14,48 +20,54 @@ var options = {
 var buildDate = new Intl.DateTimeFormat('sv-SE', options).format(new Date());
 
 module.exports = {
-	context: __dirname + "/src",
-	entry: "./app.jsx",
+    context: src,
+    entry: "./app.jsx",
 
-	// Compile into a js.app
-	output:
-	{
-	    filename: "app.js",
-            publicPath: "/js/",
-	    path: __dirname + "/dist/js",
-	},
-
-	// Include *.js and *.jsx files
-	resolve: {
-		extensions: ["", ".js", ".jsx"]
-	},
-
-	// Preprocess *.jsx files
-	module: {
-		loaders:
-		[
-			{
-				test: /\.jsx?$/,
-				exclude: /node_modules/,
-				loader: "babel-loader",
-				query: {
-					presets: ["es2015", "react"]
-				}
-			},
-		],
-	},
-
-	// Include build information (build date, git hash)
-	plugins: [
-		new webpack.DefinePlugin({
-			__COMMIT_HASH__: JSON.stringify(commitHash),
-			__BUILD_DATE__: JSON.stringify(buildDate),
-		})
-	],
-    devServer: {
-        host: "0.0.0.0",
-        contentBase: "./dist",
-        hot: true,
-    }
+    // Compile into a js.app
+    output:
+    {
+	filename: "app.js",
+        publicPath: "js",
+	path: path.resolve(__dirname, "dist/js"),
+    },
     
+    // Preprocess *.jsx files
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ]
+    },
+
+    // Files to include.
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+
+    // Include build information (build date, git hash)
+    plugins: [
+        new webpack.DefinePlugin({
+            __COMMIT_HASH__: JSON.stringify(commitHash),
+            __BUILD_DATE__: JSON.stringify(buildDate),
+        })
+    ],
+    
+    mode: mode,
+    
+    // Config for webpack-serve.
+    serve: {
+        content: "dist",
+        dev: {
+            publicPath: "/js",
+        }
+    },
+
+// TODO     devServer: {
+// TODO         host: "0.0.0.0",
+// TODO         contentBase: "./dist",
+// TODO         hot: true,
+// TODO     }
 }
