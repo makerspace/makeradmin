031d87a7d2150eb2f448cee6e909e67a8f4310a6
Aron Granberg <aron.granberg@gmail.com>
2018-08-16 17:02:08 +0200

    Do not assume any particular project name in db_init

--- db_init.py
+++ db_init.py
@@ -7,20 +7,22 @@ import multiprocessing
 containers = ["api-gateway", "membership", "messages", "economy", "rfid", "webshop"]
 
 parser = argparse.ArgumentParser(description='Initialize the database and run migrations')
-parser.add_argument('--project-name','-p', help='Use a different docker composer project name', default="makeradmin")
+parser.add_argument('--project-name','-p', help='Use a different docker composer project name', default=None)
 parser.add_argument('--containers','-c', nargs="*", default=containers, help='All containers to update')
 args = parser.parse_args()
 
+project_name_args = ["-p", args.project_name] if args.project_name is not None else []
 containers = args.containers
 
-call(["docker-compose", "-p", args.project_name, "up", "-d", "db2"])
+call(["docker-compose", *project_name_args, "up", "-d", "db2"])
 
 print("Waiting for database", end="")
 inner_bash = 'while ! mysql -uroot --password="${MYSQL_ROOT_PASSWORD}" -e "" &> /dev/null; do printf "." && sleep 1; done'
-call(["docker-compose", "-p", args.project_name, "exec", "db2", "bash", "-c", inner_bash])
+call(["docker-compose", *project_name_args, "exec", "db2", "bash", "-c", inner_bash])
 
 # Make sure all docker containers are created (but not necessarily started)
-call(["docker-compose", "-p", args.project_name, "up", "--no-start"])
+# This is important when running unit tests.
+call(["docker-compose", *project_name_args, "up", "--no-start"])
 
 print(" done")
 
@@ -28,9 +30,14 @@ sleep(1)
 
 
 def init_container(container):
-    # This is how the names appear to be formatted.
-    # If necessary 'docker-compose ps -q {container}' can be used, but that is a lot slower
-    container_name = f"{args.project_name}_{container}_1"
+    if args.project_name is not None:
+        # This is how the names are formatted according to the documentation
+        container_name = f"{args.project_name}_{container}_1"
+    else:
+        # This call takes quite a long time (0.7 seconds on my machine)
+        # which adds up to a lot when running tests, therefore a shortcut is used above if possible.
+        container_name = check_output(["docker-compose", *project_name_args, "ps", "-q", container]).decode('utf-8').strip()
+
     print(f"Initializing database for {container}...")
 
     # Get environment variables for the docker container
@@ -51,7 +58,7 @@ def init_container(container):
     FLUSH PRIVILEGES;"
     mysql -uroot --password="${{MYSQL_ROOT_PASSWORD}}" -e "FLUSH PRIVILEGES;"
     """
-    o1 = check_output(["docker-compose", "-p", args.project_name, "exec", "db2", "bash", "-c", inner_bash3], stderr=STDOUT)
+    o1 = check_output(["docker-compose", *project_name_args, "exec", "db2", "bash", "-c", inner_bash3], stderr=STDOUT)
     o1 = o1.decode('utf-8').strip().replace('\r', '')
     o1 = o1.replace("mysql: [Warning] Using a password on the command line interface can be insecure.", "").strip()
     if o1 != "":
@@ -63,7 +70,7 @@ def init_container(container):
         else echo \"No migration script found. Skipping migration for {container}\";
         fi
     """
-    call(["docker-compose", "-p", args.project_name, "run", "--rm", "--no-deps", container, "bash", "-c", migrate])
+    call(["docker-compose", *project_name_args, "run", "--rm", "--no-deps", container, "bash", "-c", migrate])
 
 for container in containers:
     init_container(container)
