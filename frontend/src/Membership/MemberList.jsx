b4d5cef9b13cb4ddd1c6490daba25a242742d755
Anders Roos <anders.roos@gmail.com>
2018-08-26 23:44:04 +0200

    #18 Simplified code.

--- frontend/src/Membership/MemberList.jsx
+++ frontend/src/Membership/MemberList.jsx
@@ -12,17 +12,6 @@ class MemberList extends React.Component {
         this.state = this.props.filters || {};
     }
     
-	updateFilters(newFilter) {
-		let filters = this.overrideFiltersFromProps(newFilter);
-		this.setState({
-			filters: filters
-		});
-	}
-
-	overrideFiltersFromProps(filters) {
-        return filters;
-	}
-	
 	render() {
 		return (
 			<div>
@@ -31,7 +20,7 @@ class MemberList extends React.Component {
 				<p className="uk-float-left">På denna sida ser du en lista på samtliga medlemmar.</p>
 				<Link to="/membership/membersx/add" className="uk-button uk-button-primary uk-float-right"><i className="uk-icon-plus-circle"/> Skapa ny medlem</Link>
 
-				<TableFilterBox onChange={this.updateFilters} />
+				<TableFilterBox onChange={(filters) => this.setState({filters})} />
 				<Members type={MemberCollection} filters={this.state.filters} />
 			</div>
 		);
