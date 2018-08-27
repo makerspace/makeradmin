00054cb3c14a5d59026bf20e10dc089365150011
Anders Roos <anders.roos@gmail.com>
2018-08-27 23:03:24 +0200

    #18 Bare member list not working.

--- frontend/src/Membership/MemberList.jsx
+++ frontend/src/Membership/MemberList.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router';
-import MemberCollection from './Collections/Member';
-import Members from './Components/Tables/Members';
 
 
 class SearchBox extends React.Component {
@@ -29,14 +27,46 @@ class SearchBox extends React.Component {
 }
 
 
+class Item extends React.Component {
+    
+    render() {
+    }
+}
+
+
+class Table extends React.Component {
+    
+    render() {
+    }
+}
+
+
 class MemberList extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = this.props.filters || {};
+        this.state = {members: []};
+        this.filters = {};
+        this.sort = null;
+        this.fetch();
+    }
+
+    sort(key) {
+        this.sort = key;
+        this.fetch();
+    }
+    
+    filter(filters) {
+        this.filters = filters;
+        this.fetch();
+    }
+    
+    fetch() {
     }
     
 	render() {
+        const members = this.state.members.map(m => <Item key={m.id} member={m}/>);
+  
 		return (
 			<div>
 				<h2>Medlemmar</h2>
@@ -45,7 +75,8 @@ class MemberList extends React.Component {
 				<Link to="/membership/membersx/add" className="uk-button uk-button-primary uk-float-right"><i className="uk-icon-plus-circle"/> Skapa ny medlem</Link>
 
 				<SearchBox onChange={(filters) => this.setState({filters})} />
-				<Members type={MemberCollection} filters={this.state.filters} />
+                <Table columns={headers} onSort={() => 1} rows={members}/>
+				{/*<Members type={MemberCollection} filters={this.state.filters} />*/}
 			</div>
 		);
 	}
@@ -53,3 +84,89 @@ class MemberList extends React.Component {
 }
 
 export default MemberList;
+
+/*
+
+import MemberCollection from './Collections/Member';
+import Members from './Components/Tables/Members';
+
+module.exports = React.createClass({
+	mixins: [Backbone.React.Component.mixin, BackboneTable],
+
+	getInitialState: function()
+	{
+		return {
+			columns: 7,
+		};
+	},
+
+	componentWillMount: function()
+	{
+		this.fetch();
+	},
+
+	removeTextMessage: function(member)
+	{
+		return "Are you sure you want to remove member \"" + member.firstname + " " + member.lastname + "\"?";
+	},
+
+	removeErrorMessage: function()
+	{
+		UIkit.notify("Ett fel uppstod vid borttagning av medlem", {timeout: 0, status: "danger"});
+	},
+
+	renderRow: function(row, i)
+	{
+		return (
+			<tr key={i}>
+				<td><Link to={"/membership/members/" + row.member_id}>{row.member_number}</Link></td>
+				<td>-</td>
+				<td>{row.firstname}</td>
+				<td>{row.lastname}</td>
+				<td>{row.email}</td>
+				<td><DateField date={row.created_at} /></td>
+				<td>
+					<TableDropdownMenu>
+						<Link to={"/membership/members/" + row.member_id}><i className="uk-icon-cog"></i> Redigera medlem</Link>
+						{this.removeButton(i, "Ta bort medlem")}
+					</TableDropdownMenu>
+				</td>
+			</tr>
+		);
+	},
+
+	renderHeader: function()
+	{
+		return [
+			{
+				title: "#",
+				sort: "member_id",
+			},
+			{
+				title: "Kön",
+			},
+			{
+				title: "Förnamn",
+				sort: "firstname",
+			},
+			{
+				title: "Efternamn",
+				sort: "lastname",
+			},
+			{
+				title: "E-post",
+				sort: "email",
+			},
+			{
+				title: "Blev medlem",
+				sort: "created_at",
+			},
+			{
+				title: "",
+			},
+		];
+	},
+});
+
+
+*/
