8848ff99d92b00fd1d44655a00e306c396c48184
Anders Roos <anders.roos@gmail.com>
2018-08-27 21:37:36 +0200

    #18 Used a simpler search box without the buggy parts.

--- frontend/src/Membership/MemberList.jsx
+++ frontend/src/Membership/MemberList.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router';
 import MemberCollection from './Collections/Member';
-import TableFilterBox from '../TableFilterBox';
 import Members from './Components/Tables/Members';
+
+
+class SearchBox extends React.Component {
+    
+    constructor(props) {
+        super(props);
+    }
+    
+	render() {
+		return (
+			<div className="filterbox">
+				<div className="uk-grid">
+					<div className="uk-width-2-3">
+						<form className="uk-form">
+							<div className="uk-form-icon">
+								<i className="uk-icon-search"/>
+								<input ref="search" type="text" className="uk-form-width-large" placeholder="Skriv in ett sökord" onChange={() => this.props.onChange({search: this.refs.search.value})} />
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
 
 
 class MemberList extends React.Component {
@@ -20,7 +44,7 @@ class MemberList extends React.Component {
 				<p className="uk-float-left">På denna sida ser du en lista på samtliga medlemmar.</p>
 				<Link to="/membership/membersx/add" className="uk-button uk-button-primary uk-float-right"><i className="uk-icon-plus-circle"/> Skapa ny medlem</Link>
 
-				<TableFilterBox onChange={(filters) => this.setState({filters})} />
+				<SearchBox onChange={(filters) => this.setState({filters})} />
 				<Members type={MemberCollection} filters={this.state.filters} />
 			</div>
 		);
