92104c05372998dc6309494dd35c47824b189995
Anders Roos <anders.roos@gmail.com>
2018-09-12 22:16:57 +0200

    #18 Added input as input2.

---
+++ frontend/src/Components/Form/Input2.jsx
@@ -0,0 +1,57 @@
+import * as React from "react";
+import classNames from 'classnames/bind';
+
+
+// TODO Rename to Input when Input is no longer used.
+export default class Input2 extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+            selected: false,
+            isDirty: false,
+        };
+    }
+
+    componentDidMount() {
+        const {model, name} = this.props;
+        this.unsubscribe = model.subscribe(() => this.setState({value: model[name] === null ? '' : model[name], isDirty: model.isDirty(name)}));
+    }
+    
+    componentWillUnmount() {
+        this.unsubscribe();
+    }
+    
+    render() {
+        const {value, selected, isDirty} = this.state;
+        const {model, name, title, icon, disabled, placeholder, formrow} = this.props;
+        
+        const classes = classNames(name,
+                                   {
+                                       "uk-form-row": formrow,
+                                       "selected": selected,
+                                       "changed": isDirty,
+                                   });
+        
+        const input = <input id={name} name={name} type="text" placeholder={placeholder || title} className="uk-form-width-large"
+                             value={value} disabled={disabled}
+                             onChange={(event) => model[name] = event.target.value}
+                             onFocus={() => this.setState({selected: true})}
+                             onBlur={() => this.setState({selected: false})}/>;
+        
+        return (
+            <div className={classes}>
+                <label htmlFor={name} className="uk-form-label">{title}</label>
+                <div className="uk-form-controls">
+                    {icon ? <div className="uk-form-icon"><i className={"uk-icon-" + icon}/>{input}</div> : input}
+                </div>
+            </div>
+        );
+    }
+}
+
+Input2.defaultProps = {
+    formrow: true,
+};
+
--- frontend/src/Membership/Components/MemberForm.js
+++ frontend/src/Membership/Components/MemberForm.js
@@ -1,62 +1,8 @@
 import React from 'react';
-// TODO Replace global input if it works.
-// import Input from '../../Components/Form/Input';
-import classNames from 'classnames/bind';
 import CountryDropdown from '../../CountryDropdown';
 import DateTime from "../../Components/Form/DateTime";
+import Input2 from "../../Components/Form/Input2";
 
-
-class Input extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: '',
-            selected: false,
-            isDirty: false,
-        };
-    }
-
-    componentDidMount() {
-        const {model, name} = this.props;
-        this.unsubscribe = model.subscribe(() => this.setState({value: model[name] === null ? '' : model[name], isDirty: model.isDirty(name)}));
-    }
-    
-    componentWillUnmount() {
-        this.unsubscribe();
-    }
-    
-    render() {
-        const {value, selected, isDirty} = this.state;
-        const {model, name, title, icon, disabled, placeholder, formrow} = this.props;
-        
-        const classes = classNames(name,
-                                   {
-                                       "uk-form-row": formrow,
-                                       "selected": selected,
-                                       "changed": isDirty,
-                                   });
-        
-        const input = <input id={name} name={name} type="text" placeholder={placeholder || title} className="uk-form-width-large"
-                             value={value} disabled={disabled}
-                             onChange={(event) => model[name] = event.target.value}
-                             onFocus={() => this.setState({selected: true})}
-                             onBlur={() => this.setState({selected: false})}/>;
-        
-        return (
-            <div className={classes}>
-                <label htmlFor={name} className="uk-form-label">{title}</label>
-                <div className="uk-form-controls">
-                    {icon ? <div className="uk-form-icon"><i className={"uk-icon-" + icon}/>{input}</div> : input}
-                </div>
-            </div>
-        );
-    }
-}
-
-Input.defaultProps = {
-    formrow: true,
-};
 
 // TODO Maybe not really a reusable component, check usages later.
 export default class MemberForm extends React.Component {
@@ -78,7 +24,7 @@ export default class MemberForm extends React.Component {
     }
 
     render() {
-        const {member, onCancel, onSave, onRemove} = this.props;
+        const {member, onSave, onRemove} = this.props;
         const {saveDisabled} = this.state;
         
 		return (
@@ -87,20 +33,20 @@ export default class MemberForm extends React.Component {
 					<fieldset >
 						<legend><i className="uk-icon-user"/> Personuppgifter</legend>
 
-						<Input model={member} name="civicregno" title="Personnummer" />
-						<Input model={member} name="firstname" title="Förnamn" />
-						<Input model={member} name="lastname" title="Efternamn" />
-						<Input model={member} name="email" title="E-post" />
-						<Input model={member} name="phone" title="Telefonnummer" />
+						<Input2 model={member} name="civicregno" title="Personnummer" />
+						<Input2 model={member} name="firstname" title="Förnamn" />
+						<Input2 model={member} name="lastname" title="Efternamn" />
+						<Input2 model={member} name="email" title="E-post" />
+						<Input2 model={member} name="phone" title="Telefonnummer" />
 					</fieldset>
 
 					<fieldset data-uk-margin>
 						<legend><i className="uk-icon-home"/> Adress</legend>
 
-						<Input model={member} name="address_street" title="Address" />
-						<Input model={member} name="address_extra" title="Address extra" placeholder="Extra adressrad, t ex C/O adress" />
-						<Input model={member} name="address_zipcode" title="Postnummer" />
-						<Input model={member} name="address_city" title="Postort" />
+						<Input2 model={member} name="address_street" title="Address" />
+						<Input2 model={member} name="address_extra" title="Address extra" placeholder="Extra adressrad, t ex C/O adress" />
+						<Input2 model={member} name="address_zipcode" title="Postnummer" />
+						<Input2 model={member} name="address_city" title="Postort" />
 
 						<div className="uk-form-row">
 							<label htmlFor="" className="uk-form-label">Land</label>
