286e92d4d69bbf1563095f52b526ae704b3f9820
Anders Roos <anders.roos@gmail.com>
2018-09-22 23:22:46 +0200

    #18 Simplified member login.

--- frontend/src/User/Member.jsx
+++ frontend/src/User/Member.jsx
@@ -1,55 +1,39 @@
 import React from 'react';
-import { withRouter } from 'react-router'
+import MemberLogin from './MemberLogin';
+import MemberView from './MemberView';
+import auth from '../auth';
+import { browserHistory } from 'react-router';
 
-import MemberLogin from './MemberLogin'
-import MemberView from './MemberView'
-import auth from '../auth'
-import { browserHistory } from 'react-router'
 
-module.exports = withRouter(class Member extends React.Component
-{
-	constructor(props)
-	{
-		super(props);
-		this.state = { isLoggedIn: false };
-	}
+const logout = () => {
+    auth.logout();
+    browserHistory.push("/member");
+};
 
-	updateAuth()
-	{
-		this.setState({
-			isLoggedIn: auth.isLoggedIn()
-		});
-	}
 
-	componentWillMount()
-	{
-		auth.onChange = () => this.updateAuth();
-		this.updateAuth();
-	}
+export default class Member extends React.Component {
+    constructor(props)
+    {
+        super(props);
+        this.state = {isLoggedIn: auth.isLoggedIn()};
+    }
 
-	logout()
-	{
-		auth.logout();
-		browserHistory.push("/member");
-	}
-
-	render()
-	{
-		if(this.state.isLoggedIn === true)
-		{
-			return (
-				<div className="uk-vertical-align uk-form-width-large uk-height-1-1" style={{marginLeft: "auto", marginRight: "auto", width: "600px"}}>
-					<div className="uk-vertical-align-middle uk-width-1-1">
-						<MemberView logout={this.logout.bind(this)}/>
-					</div>
-				</div>
-			);
-		}
-		else
-		{
-			return (
-				<MemberLogin />
-			);
-		}
-	}
-});
+    componentDidMount() {
+        auth.onChange = isLoggedIn => this.setState({isLoggedIn});
+    }
+    
+    render()
+    {
+        if (this.state.isLoggedIn) {
+            return (
+                <div className="uk-vertical-align uk-form-width-large uk-height-1-1" style={{marginLeft: "auto", marginRight: "auto", width: "600px"}}>
+                    <div className="uk-vertical-align-middle uk-width-1-1">
+                        <MemberView logout={logout}/>
+                    </div>
+                </div>
+            );
+        }
+        
+        return <MemberLogin/>;
+    }
+}
--- frontend/src/User/Routes.jsx
+++ frontend/src/User/Routes.jsx
@@ -1,7 +1,7 @@
 module.exports = {
     path: "member",
     indexRoute: {
-        component: require("./Member"),
+        component: require("./Member").default,
     },
     childRoutes: [
         {
