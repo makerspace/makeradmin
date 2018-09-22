491e552f9df6a6c78fd9e99af0a044323802ec4d
Anders Roos <anders.roos@gmail.com>
2018-09-22 20:49:38 +0200

    #18 Fixed statistics.

--- frontend/src/Statistics/Routes.jsx
+++ frontend/src/Statistics/Routes.jsx
@@ -1,10 +1,10 @@
 module.exports = {
-	childRoutes: [
-		{
-			path: "/statistics",
-			indexRoute: {
-				component: require("./Pages/Overview")
-			},
-		}
-	]
-}
+    childRoutes: [
+        {
+            path: "/statistics",
+            indexRoute: {
+                component: require("./StatisticsOverview").default
+            },
+        }
+    ]
+};
