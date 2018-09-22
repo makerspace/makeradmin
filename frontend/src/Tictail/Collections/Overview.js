9684bf231d19ff90f889e17c25404d20e1983361
Anders Roos <anders.roos@gmail.com>
2018-09-22 21:21:41 +0200

    #18 Removed tictail pages.

--- frontend/src/Tictail/Collections/Overview.js
+++
@@ -1,8 +0,0 @@
-import Backbone from 'backbone'
-import OverviewModel from '../Models/Overview'
-
-module.exports = Backbone.PageableCollection.extend(
-{
-	model: OverviewModel,
-	url: "/tictail/overview",
-});
