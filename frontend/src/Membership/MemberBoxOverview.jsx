9b43154fb7f70300e6eabb1dbb02a4895918fbe4
Anders Roos <anders.roos@gmail.com>
2018-09-25 20:02:08 +0200

    #18 Added spans list.

--- frontend/src/Membership/MemberBoxOverview.jsx
+++ frontend/src/Membership/MemberBoxOverview.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Member from "../Models/Member";
-import {withRouter} from "react-router";
 
 
 class MemberBoxOverview extends React.Component {
     render() {
-        const {router} = this.props;
+        const {member} = this.context;
         
         return (
             <div className='uk-margin-top'>
-                Översikt.
+                {member.member_number} {member.firstname} {member.lastname}
             </div>
         );
     }
@@ -21,4 +20,4 @@ MemberBoxOverview.contextTypes = {
 };
 
 
-export default withRouter(MemberBoxOverview);
+export default MemberBoxOverview;
--- frontend/src/Membership/MemberBoxSpans.jsx
+++ frontend/src/Membership/MemberBoxSpans.jsx
@@ -1,24 +1,54 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Member from "../Models/Member";
-import {withRouter} from "react-router";
+import {Link} from "react-router";
+import Collection from "../Models/Collection";
+import Span from "../Models/Span";
+import {confirmModal} from "../message";
+import CollectionTable from "../Components/CollectionTable";
+import DateTimeShow from "../Components/DateTimeShow";
+import DateShow from "../Components/DateShow";
 
 
 class MemberBoxSpans extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.collection = new Collection({type: Span, filter: {member_id: props.params.member_id}});
+    }
+    
     render() {
-        const {router} = this.props;
+        const deleteItem = item => confirmModal(item.deleteConfirmMessage()).then(() => item.del()).then(() => this.collection.fetch(), () => null);
         
         return (
-            <div className='uk-margin-top'>
-                Översikt.
+            <div className="uk-margin-top">
+                <h2>Medlemsperioder</h2>
+                <CollectionTable
+                    collection={this.collection}
+                    columns={[
+                        {title: "#", sort: "span_id"},
+                        {title: "Typ", sort: "span_type"},
+                        {title: "Skapad", sort: "created_at"},
+                        {title: ""},
+                        {title: "Raderad", sort: "deleted_at"},
+                        {title: "Start", sort: "startdate"},
+                        {title: "Slut", sort: "enddate"},
+                    ]}
+                    rowComponent={({item}) => (
+                        <tr>
+                            <td><Link to={"/membership/spans/" + item.id}>{item.id}</Link></td>
+                            <td><Link to={"/membership/spans/" + item.id}>{item.span_type}</Link></td>
+                            <td><DateTimeShow date={item.created_at}/></td>
+                            <td>{item.creation_reason}</td>
+                            <td><DateTimeShow date={item.deleted_at}/></td>
+                            <td><DateShow date={item.startdate}/></td>
+                            <td><DateShow date={item.enddate}/></td>
+                            <td><a onClick={() => deleteItem(item)} className="removebutton"><i className="uk-icon-trash"/></a></td>
+                        </tr>
+                    )}
+                />
             </div>
         );
     }
 }
 
-MemberBoxSpans.contextTypes = {
-    member: PropTypes.instanceOf(Member)
-};
 
-
-export default withRouter(MemberBoxSpans);
+export default MemberBoxSpans;
