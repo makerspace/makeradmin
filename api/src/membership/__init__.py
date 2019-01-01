732f53f165528ba6f491f93bfb63b7ab8629b784
Anders Roos <anders.roos@gmail.com>
2019-01-01 17:22:54 +0100

    #43 Started on membership conversion.

--- api/src/membership/__init__.py
+++ api/src/membership/__init__.py
@@ -1,5 +1,6 @@
 from service.config import config
 from service.external_service import ExternalService
-
+from service.internal_service import InternalService
 
 service = ExternalService(name='membership', url=config.get('MEMBERSHIP_URL'))
+# service = InternalService(name='membership', migrations=True)
