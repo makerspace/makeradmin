30e8ade075a574e2fb0c91d2c95002a3bb328e5c
Anders Roos <anders.roos@gmail.com>
2019-01-08 21:05:08 +0100

    #44 Fixed todo.

--- api/src/membership/__init__.py
+++ api/src/membership/__init__.py
@@ -2,8 +2,7 @@ from service.config import config
 from service.external_service import ExternalService
 from service.internal_service import InternalService
 
-# TODO
-# service = ExternalService(name='membership', url=config.get('MEMBERSHIP_URL'))
+# TODO service = ExternalService(name='membership', url=config.get('MEMBERSHIP_URL'))
 service = InternalService(name='membership', migrations=True)
 
 
