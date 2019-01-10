07f4396e76b1901286f5c379320fb6cfa320e9d4
Anders Roos <anders.roos@gmail.com>
2019-01-10 21:49:25 +0100

    #43 Fixed tests.

--- test/src/aid/test/selenium.py
+++ test/src/aid/test/selenium.py
@@ -96,5 +96,5 @@ class SeleniumTest(ApiTest):
 
     def browse_shop(self):
         self.webdriver.get(f"{self.public_url}/shop")
-        self.wait_for_page(title="Makerspace Webshop")
+        self.wait_for_page(title="Stockholm Makerspace Webshop")
         
