e6fc54189fbb13a2eedf2c4a024b4e123b77e69b
Anders Roos <anders.roos@gmail.com>
2019-02-11 23:35:55 +0100

    #51 Implemented memberdata endpoint in python.

---
+++ api/src/systest/api/multiaccess_test.py
@@ -0,0 +1,20 @@
+from test_aid.systest_base import ApiTest
+
+
+class Test(ApiTest):
+    
+    def test_not_included_if_has_no_span(self):
+        " TODO "
+        
+    def test_not_included_if_has_no_key(self):
+        " TODO "
+        
+    def test_not_included_if_has_only_deleted_span(self):
+        " TODO "
+
+    def test_not_included_if_has_only_membership_span(self):
+        " TODO "
+    
+    def test_not_included_if_has_only_deleted_key(self):
+        " TODO "
+        
