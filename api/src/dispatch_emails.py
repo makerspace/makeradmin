4c06386809df9713e8488d382d545611e7902049
Anders Roos <anders.roos@gmail.com>
2019-02-12 20:48:07 +0100

    #53 Implementing sending of emails from python.

--- api/src/dispatch_emails.py
+++ api/src/dispatch_emails.py
@@ -1,7 +1,9 @@
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from contextlib import closing
+from datetime import date
 from time import sleep
 
+import requests
 from rocky.process import log_exception, stoppable
 from sqlalchemy.orm import sessionmaker
 
@@ -16,82 +18,46 @@ def send_messages(db_session, key, domain, sender, to_override, limit):
     query = query.filter(Message.message_type == 'email', Recipient.status == 'queued')
     query = query.limit(limit)
     
-    logger.info(query.all())
+    for recipient in query:
+        to = recipient.recipient
+        recipient_id = recipient.messages_recipient_id
+        msg = f"sending {recipient_id} to {to}"
+        
+        if to_override:
+            msg += f" (overriding to {to_override})"
+            to = to_override
 
-# 			$messages = DB::table("messages_recipient")
-# 				->leftJoin("messages_message", "messages_message.messages_message_id", "=", "messages_recipient.messages_message_id")
-# 				->selectRaw("messages_message.messages_message_id AS message_id")
-# 				->selectRaw("messages_recipient.messages_recipient_id AS recipient_id")
-# 				->selectRaw("messages_recipient.title AS subject")
-# 				->selectRaw("messages_recipient.description AS body")
-# 				->selectRaw("messages_recipient.recipient AS recipient")
-# 				->selectRaw("messages_recipient.member_id AS member_id")
-# 				->where("messages_recipient.status", "=", "queued")
-# 				->where("messages_message.message_type", "=", "email")
-# 				->limit($mailgun_limit)
-# 				->get();
-# 
-# 			// Create an clean array with data
-# 			$list = [];
-# 			foreach($messages as $message)
-# 			{
-# 
-# 				try {
-# 					// Get the member's email
-# 					$email = $message->recipient;
-# 
-# 					// Override recipient
-# 					if (!empty($mailgun_to_override)) {
-# 						$email = $mailgun_to_override;
-# 						echo "Sending mail to {$message->recipient} (overriding to $email)\n";
-# 					}
-# 					else {
-# 						echo "Sending mail to {$message->recipient}\n";
-# 					}
-# 
-# 					// Send the mail via Mailgun
-# 					$result = $mgClient->sendMessage($mailgun_domain,
-# 						array(
-# 							'from'    => $mailgun_from,
-# 							'to'      => $email,
-# 							'subject' => $message->subject,
-# 							'html'    => $message->body
-# 						)
-# 					);
-# 
-# 					// Update the database and flag the E-mail as sent to this particular recipient
-# 					DB::table("messages_recipient")
-# 						->where("messages_recipient_id", $message->recipient_id)
-# 						->update([
-# 							"status"    => "sent",
-# 							"date_sent" => date("Y-m-d H:i:s"),
-# 						]
-# 					);
-# 
-# 					// todo: Uppdatera status i messages
-# 				}
-# 				catch(\Exception $e)
-# 				{
-# 					DB::table("messages_recipient")
-# 						->where("messages_recipient_id", $message->recipient_id)
-# 						->update([
-# 							"status"    => "failed",
-# 							"date_sent" => date("Y-m-d H:i:s"),
-# 						]
-# 					);
-# 
-# 					// todo: Error handling
-# 					echo "Error\n";
-# 				}
-# 			}
+        logger.info(msg)
 
-
+        response = requests.post(f"https://api.mailgun.net/v3/{domain}/messages", auth=('api', key),
+                                 data={
+                                     'from': sender,
+                                     'to': to,
+                                     'subject': recipient.title,
+                                     'text': recipient.description,
+                                 })
+        
+        if response.ok:
+            recipient.status = 'sent'
+            recipient.date_sent = date.today()
+            
+            db_session.add(recipient)
+            db_session.commit()
+            
+        else:
+            recipient.status = 'failed'
+            recipient.date_sent = date.today()
+            
+            db_session.add(recipient)
+            db_session.commit()
+            
+            logger.error(f"failed to send {recipient_id} to {to}: {response.content}")
 
 
 if __name__ == '__main__':
 
     with log_exception(status=1), stoppable():
-        parser = ArgumentParser(description="Dispatch emails in db send queue.", 
+        parser = ArgumentParser(description="Dispatch emails in db send queue.",
                                 formatter_class=ArgumentDefaultsHelpFormatter)
         
         parser.add_argument('--sleep', default=4, help='Sleep time (in seconds) between checking for messages to send.')
@@ -104,28 +70,12 @@ if __name__ == '__main__':
         
         logger.info(f'checking for emails to send every {args.sleep} seconds, limit is {args.limit}')
         
-        key = config.get('MAILGUN_KEY')
+        key = config.get('MAILGUN_KEY', log_value=False)
         domain = config.get('MAILGUN_DOMAIN')
         sender = config.get('MAILGUN_FROM')
         to_override = config.get('MAILGUN_TO_OVERRIDE')
         
         while True:
+            sleep(args.sleep)
             with closing(session_factory()) as db_session:
                 send_messages(db_session, key, domain, sender, to_override, args.limit)
-            sleep(args.sleep)
-            
-
-# 		// Instantiate the Mailgunclient
-# 		$mgClient = new Mailgun(config("mailgun.key"));
-# 		$mailgun_domain = config("mailgun.domain");
-# 		$mailgun_from = config("mailgun.from");
-# 		$mailgun_to_override = config("mailgun.to");
-# 		$mailgun_limit = 10;
-# 
-# 		while(true) {
-# 			sleep(4.0);
-# 
-# 			// Get all messages from database
-# 		}
-# 	}
-# }
