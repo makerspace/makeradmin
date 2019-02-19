1177b2ad4ee0f389bf789b174aa6f4d541c767b4
Aron Granberg <aron.granberg@gmail.com>
2019-02-19 21:02:45 +0100

    Add search to webshop. Fixes #59.

---
+++ public/ts/shopsearch.ts
@@ -0,0 +1,40 @@
+export function search(items: any[], query: string) {
+	query = query.toLowerCase().trim();
+	if (query === "") {
+		return items;
+	}
+
+	const keys = query.split(" ");
+	const scores = [];
+	for (const item of items) {
+		const nameKeys = item.name.toLowerCase().trim().split(" ");
+
+		let score = 0;
+		for (const queryKey of keys) {
+			let matches = 0;
+			for (const itemKey of nameKeys) {
+				if (itemKey.includes(queryKey)) {
+					matches = Math.max(matches, 1);
+				}
+				if (itemKey === queryKey) {
+					matches = Math.max(matches, 2);
+				}
+			}
+
+			score += matches;
+		}
+
+		if (score > 0) {
+			scores.push({ score: score, item: item });
+		}
+	}
+
+	scores.sort((a,b) => b.score - a.score);
+
+	items = [];
+	for (const item of scores) {
+		items.push(item.item);
+	}
+
+	return items;
+}
