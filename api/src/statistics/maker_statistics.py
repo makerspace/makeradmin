a03c3dd4b0cf6701638f976d17d8e7e559b8dcfc
Anders Roos <anders.roos@gmail.com>
2019-04-02 21:27:43 +0200

    #75 Make maker statistics work with sqlalchemy.

--- api/src/statistics/maker_statistics.py
+++ api/src/statistics/maker_statistics.py
@@ -1,27 +1,9 @@
-#
-# This file doesn't really belong in the webshop, but it does share a significant amount of boilerplate
-# so it is easy to just put it here.
-#
-
-# Note: the file is named maker_statistics instead of just statistics because python has a built in module called statistics
-
-import backend_service
-from backend_service import route_helper
-from typing import List, Tuple
 from datetime import datetime
-instance = backend_service.create(name="statistics", url="statistics", port=80, version="1.0")
+from typing import List, Tuple
 
-# Grab the database so that we can use it inside requests
-db = instance.db
-
-
-@instance.route("membership/by_date", methods=["GET"], permission=None)
-@route_helper
-def membership_by_date_statistics():
-    return {
-        "membership": spans_by_date("membership"),
-        "labaccess": spans_by_date("labaccess"),
-    }
+from service.db import db_session
+from service.logging import logger
+from shop.models import Product
 
 
 def spans_by_date(span_type) -> List[Tuple[str, int]]:
@@ -31,56 +13,52 @@ def spans_by_date(span_type) -> List[Tuple[str, int]]:
     SELECT date, count(distinct(member_id)) AS labmembers
         FROM membership_spans as ms
         JOIN (
-            (SELECT enddate AS date FROM membership_spans WHERE type = %s AND deleted_at IS NULL)
+            (SELECT enddate AS date FROM membership_spans WHERE type = :span_type AND deleted_at IS NULL)
             UNION DISTINCT
-            (SELECT DATE_ADD(enddate, INTERVAL 1 DAY) as date FROM membership_spans WHERE type = %s AND deleted_at IS NULL)
+            (SELECT DATE_ADD(enddate, INTERVAL 1 DAY) as date FROM membership_spans
+               WHERE type = :span_type AND deleted_at IS NULL)
             UNION DISTINCT
-            (SELECT startdate FROM membership_spans WHERE type = %s AND deleted_at IS NULL)
+            (SELECT startdate FROM membership_spans
+                WHERE type = :span_type AND deleted_at IS NULL)
             UNION DISTINCT
-            (SELECT DATE_SUB(startdate, INTERVAL 1 DAY) FROM membership_spans WHERE type = %s AND deleted_at IS NULL)
+            (SELECT DATE_SUB(startdate, INTERVAL 1 DAY) FROM membership_spans
+               WHERE type = :span_type AND deleted_at IS NULL)
         ) AS dates
         ON (
             ms.startdate <= dates.date AND
             dates.date <= ms.enddate
         )
         WHERE (
-            ms.type = %s AND
+            ms.type = :span_type AND
             ms.deleted_at IS NULL
         )
         GROUP BY date
         ORDER BY date;"""
+    
+    dates = db_session.execute(query, {'span_type': span_type})
 
-    with db.cursor() as cur:
-        cur.execute(query, (span_type, span_type, span_type, span_type, span_type))
-        dates: List[Tuple[datetime, int]] = cur.fetchall()
+    dates_str = [(date.strftime("%Y-%m-%d"), count) for (date, count) in dates]
 
-        # new_dates = []
-        # for date in dates:
-        #   while len(new_dates) > 0 and round((date[0] - new_dates[-1][0]).total_seconds()/(24*3600)) > 1:
-        #       new_dates.append((new_dates[-1][0] + timedelta(days=1), new_dates[-1][1]))
-        #   new_dates.append(date)
-
-        dates_str = [(date.strftime("%Y-%m-%d"), count) for (date, count) in dates]
-
-        return dates_str
+    return dates_str
 
 
-@instance.route("lasertime/by_month", methods=["GET"], permission=None)
-@route_helper
+def membership_by_date_statistics():
+    return {
+        "membership": spans_by_date("membership"),
+        "labaccess": spans_by_date("labaccess"),
+    }
+
+
 def lasertime():
-    with db.cursor() as cur:
-        cur.execute("SELECT id FROM webshop_products WHERE name='Använding av Laserskärare'")
-        id = cur.fetchone()
-        if id is None:
-            abort(500, "Kunde inte hitta laserskärningstidsprodukten")
-
-        cur.execute("""
-            SELECT DATE_FORMAT(webshop_transactions.created_at, "%%Y-%%m"), sum(webshop_transaction_contents.count) FROM webshop_transaction_contents
+    query = db_session.execute("""
+            SELECT DATE_FORMAT(webshop_transactions.created_at, "%Y-%m"), sum(webshop_transaction_contents.count)
+            FROM webshop_transaction_contents
             INNER JOIN webshop_transactions
             ON webshop_transactions.id = webshop_transaction_contents.transaction_id
-            WHERE webshop_transaction_contents.product_id=%s AND webshop_transactions.status='completed'
-            GROUP BY DATE_FORMAT(webshop_transactions.created_at, "%%Y-%%m")
-            """, id)
-        results = cur.fetchall()
-        results = [(date, int(count)) for (date, count) in results]
-        return results
+            WHERE webshop_transaction_contents.product_id=7 AND webshop_transactions.status='completed'
+            GROUP BY DATE_FORMAT(webshop_transactions.created_at, "%Y-%m")
+            """)
+    
+    results = [(date, int(count)) for (date, count) in query]
+    logger.info(results)
+    return results
--- api/src/statistics/views.py
+++ api/src/statistics/views.py
@@ -1,12 +1,15 @@
 from service.api_definition import GET, PUBLIC
 from statistics import service
+from statistics.maker_statistics import membership_by_date_statistics, lasertime
 
 
 @service.route("/membership/by_date", method=GET, permission=PUBLIC)
-def membership_by_date_statistics():
-    return {
-        "membership": spans_by_date("membership"),
-        "labaccess": spans_by_date("labaccess"),
-    }
+def membership_by_date_statistics_route():
+    return membership_by_date_statistics()
+
+
+@service.route("/lasertime/by_month", method=GET, permission=PUBLIC)
+def lasertime_route():
+    return lasertime()
 
 
