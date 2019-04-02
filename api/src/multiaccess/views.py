b1e6d16344f856d8ba7c991979ca4627d83595ae
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2019-04-02 20:42:38 +0200

    Setting end date to None if there are no spans

--- api/src/multiaccess/views.py
+++ api/src/multiaccess/views.py
@@ -12,7 +12,7 @@ def member_to_response_object(member):
         'member_number': member.member_number,
         'firstname': member.firstname,
         'lastname': member.lastname,
-        'end_date': max((span.enddate for span in member.spans)).isoformat() if len(member.spans) > 0 else member.created_at,
+        'end_date': max((span.enddate for span in member.spans)).isoformat() if len(member.spans) > 0 else None,
         'keys': [{'key_id': key.key_id, 'rfid_tag': key.tagid} for key in member.keys],
     }
 
