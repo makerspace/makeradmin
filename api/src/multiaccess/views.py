6b60b1b91a8c8bedf64f126c69b97c550c1d3417
Julita Barkauskaite <julita.barkauskaite@daresay.co>
2019-04-14 15:49:49 +0200

    Changed to spaces instead of tabs, on popular demand

--- api/src/multiaccess/views.py
+++ api/src/multiaccess/views.py
@@ -96,7 +96,7 @@ def box_terminator_validate(member_number=Arg(int), box_label_id=Arg(int)):
         status = "terminate"
         
     return {
-    	"member_number": member.member_number,
+        "member_number": member.member_number,
         "name": f"{member.firstname} {member.lastname or ''}",
         "expire_date": expire_date.isoformat(),
         "terminate_date": terminate_date.isoformat(),
