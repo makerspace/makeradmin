979103890471e3aa44fa1ba616aaacafd0616d7e
Anders Roos <anders.roos@gmail.com>
2019-05-21 22:29:56 +0200

    Moved prod config changes into git.

--- create_connect.sh
+++ create_connect.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-docker-compose -f docker-compose.yml up --no-start api admin public && \
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --no-start api admin public && \
 docker network connect makeradmin-bridge `docker-compose ps -q api` && \
 docker network connect makeradmin-bridge `docker-compose ps -q admin` && \
 docker network connect makeradmin-bridge `docker-compose ps -q public`
-
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./create_connect.sh && \
-docker-compose -f docker-compose.yml up -d
+docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
