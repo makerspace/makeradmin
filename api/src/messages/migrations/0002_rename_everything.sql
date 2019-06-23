435eee72515281decb28f47bdc70a285fa61712b
Anders Roos <anders.roos@gmail.com>
2019-06-23 15:51:55 +0200

    #98 Added migration.

--- api/src/messages/migrations/0002_rename_everything.sql
+++ api/src/messages/migrations/0002_rename_everything.sql
@@ -1,10 +1,9 @@
 
 --- drop unused columns
-ALTER TABLE `message_recipient` DROP COLUMN `message_template_id`;
-ALTER TABLE `message_recipient` DROP COLUMN `message_type`;
-ALTER TABLE `message_recipient` DROP COLUMN `status`;
+ALTER TABLE `messages_recipient` DROP FOREIGN KEY `messages_recipient_messages_message_id_foreign`;
+ALTER TABLE `messages_recipient` DROP COLUMN `messages_message_id`;
 
---- drop unused template table
+--- drop unused tables
 DROP TABLE `messages_template`;
 DROP TABLE `messages_message`;
 
@@ -16,8 +15,8 @@ ALTER TABLE `message` RENAME COLUMN `messages_recipient_id` TO `id`;
 ALTER TABLE `message` RENAME COLUMN `title` TO `subject`;
 ALTER TABLE `message` RENAME COLUMN `description` TO `body`;
 
---- add purpose table to message template
-ALTER TABLE `message` ADD COLUMN `template` enum('labaccess_reminder') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+--- add template as "enum" to message, no real enum becuse too hard to maintain
+ALTER TABLE `message` ADD COLUMN `template` varchar(112) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 --- change varchar columns to text
 ALTER TABLE `message` MODIFY COLUMN `subject` text COLLATE utf8mb4_0900_ai_ci NOT NULL;
