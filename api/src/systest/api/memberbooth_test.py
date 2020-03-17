bf4fcb037302f42070d3d4e7f2717a054c452000
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2020-03-17 23:26:42 +0100

    Testing to get member by tag

--- api/src/systest/api/memberbooth_test.py
+++ api/src/systest/api/memberbooth_test.py
@@ -2,10 +2,10 @@ from test_aid.systest_base import ApiTest
 
 
 class Test(ApiTest):
-    
+
     def test_memberbooth_can_get_member_by_number_even_if_member_does_not_have_a_key(self):
         member = self.db.create_member()
-        
+
         self.api.get('/multiaccess/memberbooth/member', params=dict(member_number=member.member_number)).expect(
             200,
             data__keys=[],
@@ -16,8 +16,20 @@ class Test(ApiTest):
     def test_memberbooth_can_get_member_by_number(self):
         member = self.db.create_member()
         key = self.db.create_key()
-        
+
         response = self.api.get('/multiaccess/memberbooth/member', params=dict(member_number=member.member_number))
+        response.expect(
+            200,
+            data__firstname=member.firstname,
+            data__member_id=member.member_id,
+        )
+        self.assertNotEqual([], response.data["keys"], "There exists keys")
+
+    def test_memberbooth_can_get_member_by_tag(self):
+        member = self.db.create_member()
+        key = self.db.create_key()
+
+        response = self.api.get('/multiaccess/memberbooth/tag', params=dict(tagid=key.tagid))
         response.expect(
             200,
             data__firstname=member.firstname,
