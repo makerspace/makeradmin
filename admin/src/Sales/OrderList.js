8eae196ac96433c4c84e6ee07bcdce5c5843983b
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-01-25 23:17:39 +0100

    WIP: Adding search box without function

--- admin/src/Sales/OrderList.js
+++ admin/src/Sales/OrderList.js
@@ -5,6 +5,31 @@ import Collection from "../Models/Collection";
 import CollectionTable from "../Components/CollectionTable";
 import DateTimeShow from "../Components/DateTimeShow";
 
+class SearchBox extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="filterbox">
+                <div className="uk-grid">
+                    <div className="uk-width-2-3">
+                        <form className="uk-form">
+                            <div className="uk-form-icon">
+                                <i className="uk-icon-search"/>
+                                <input ref={c => this.search = c} tabIndex="1" type="text" className="uk-form-width-large" placeholder="Skriv in ett sökord"
+                                       onChange={() => this.props.onChange(this.search.value)} />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
 
 const Row = props => {
     const {item} = props;
@@ -39,6 +64,7 @@ class OrderList extends React.Component {
         return (
             <div className="uk-margin-top">
                 <h2>Inkommna ordrar</h2>
+                <SearchBox onChange={terms => this.collection.updateSearch(terms)} />
                 <CollectionTable emptyMessage="Ingar ordrar" rowComponent={Row} collection={this.collection} columns={columns} />
             </div>
         );
