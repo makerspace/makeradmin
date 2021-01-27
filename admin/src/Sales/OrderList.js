35216ca8fa9d38cae7cd3b8a708d79166e7a49c1
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2021-01-27 21:47:43 +0100

    Fixes: submit and start state
    - Not reloading page when pressing enter
    - Fixing warning about mixed uncontrolled / controlled component

--- admin/src/Sales/OrderList.js
+++ admin/src/Sales/OrderList.js
@@ -12,7 +12,7 @@ class SearchBox extends React.Component {
             <div className="filterbox">
                 <div className="uk-grid">
                     <div className="uk-width-2-3">
-                        <form className="uk-form">
+                        <form className="uk-form" onSubmit={(e) => e.preventDefault()}>
                             <div className="uk-form-icon">
                                 <i className="uk-icon-search"/>
                                 <input value={this.props.value} ref={c => this.search = c} tabIndex="1" type="text" className="uk-form-width-large" placeholder="Skriv in ett sökord" onChange={(e) => this.props.handleChange(e.target.value)} />
@@ -45,7 +45,7 @@ class OrderList extends React.Component {
     componentDidMount() {
         const params = new URLSearchParams(this.props.location.search);
         const search_term = params.get('search');
-        this.setState({'search': search_term});
+        this.setState({'search': search_term || ''});
         this.collection.updateSearch(search_term);
     }
 
