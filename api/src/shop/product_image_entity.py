08fc38b248c93479ca70336e7321e96d343ca7f2
Anders Roos <anders.roos@gmail.com>
2021-12-21 22:16:44 +0100

    #113 Fixed product image upload.

--- api/src/shop/product_image_entity.py
+++ api/src/shop/product_image_entity.py
@@ -10,11 +10,12 @@ class ProductImageEntity(Entity):
     
     def to_model(self, obj):
         model = super().to_model(obj)
-        
+
         if "data" in model and "type" in model:
             image = Image.open(BytesIO(model["data"]))
             image.thumbnail((500, 1_000_000), Image.ANTIALIAS)
             bytes = BytesIO()
+            image.save(bytes, format="png", compress_level=8)
             model["data"] = bytes.getvalue()
             model["type"] = "image/png"
             if len(model["data"]) > 1_000_000:
