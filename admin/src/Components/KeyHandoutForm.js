926bcdcdbc05989135529d640066b42128215013
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2022-03-12 21:30:39 +0100

    Checking if member has keys and showing text based on it

--- admin/src/Components/KeyHandoutForm.js
+++ admin/src/Components/KeyHandoutForm.js
@@ -17,8 +17,11 @@ class KeyHandoutForm extends React.Component {
         this.state = {
             can_save_member: false,
             can_save_key: false,
+            keys: [],
+            has_signed: false,
         };
         this.keyCollection = new Collection({type: Key, url: `/membership/member/${member.id}/keys`, idListName: 'keys', pageSize: 0});
+        this.signedChanged = this.signedChanged.bind(this);
     }
 
     createKey() {
@@ -30,10 +33,14 @@ class KeyHandoutForm extends React.Component {
                   });
     }
 
+    signedChanged(signed) {
+        this.setState({has_signed: signed});
+    }
+
     componentDidMount() {
         const {member} = this.props;
-        this.unsubscribe_member = member.subscribe(() => this.setState({can_save_member: member.canSave()}));
-        this.unsubscribe_keys = this.keyCollection.subscribe((keys) => this.setState({keys: keys}));
+        this.unsubscribe_member = member.subscribe(() => this.setState({can_save_member: member.canSave(), has_signed: member.civicregno.length > 0}));
+        this.unsubscribe_keys = this.keyCollection.subscribe((keys) => this.setState({keys: keys.items}));
         const key = this.key;
         this.unsubscribe_key = key.subscribe(() => this.setState({can_save_key: key.canSave()}));
     }
@@ -46,7 +53,38 @@ class KeyHandoutForm extends React.Component {
 
     render() {
         const {member, onSave, onDelete} = this.props;
-        const {can_save_member, can_save_key} = this.state;
+        const {can_save_member, can_save_key, keys, has_signed} = this.state;
+        console.log(has_signed);
+
+        // Show different content based on if the user has a key or not
+        let key_paragraph;
+        if (keys.length === 0) {
+            key_paragraph = <>
+                    <div className="uk-container">
+                        <TextInput model={this.key} tabIndex="1" name="tagid" title="RFID" placeholder="Använd en RFID-läsare för att läsa av det unika numret på nyckeln" />
+
+                        <div className="uk-form-row uk-margin-top">
+                            <div className="uk-form-controls">
+                                <button className="uk-button uk-button-success uk-float-right" disabled={!can_save_key}><i className="uk-icon-save"/> Skapa nyckel</button>
+                            </div>
+                        </div>
+                    </div>
+                </>;
+        } else if (keys.length === 1) {
+            key_paragraph = <>
+                    <div className="uk-container">
+                        Användaren har en nyckel registrerad (med id=<span style={{fontFamily: "monospace"}}>{keys[0].tagid}</span>). Kontrollera om hen vet om det och har kvar nyckeln. Gå annars till <a href={"/membership/members/" + member.id + "/keys"}>Nycklar</a> och ta bort den gamla nyckeln, och lägg till en ny.
+                    </div>
+                </>;
+        } else {
+            key_paragraph = <>
+                <div className="uk-container">
+                    <div className="uk-container">
+                        Användaren har flera nycklar registrerade! Gå till <a href={"/membership/members/" + member.id + "/keys"}>Nycklar</a> och ta bort alla nycklar utom en.
+                    </div>
+                </div>
+            </>;
+        }
 
         return (
         <div className="meep">
@@ -57,7 +95,7 @@ class KeyHandoutForm extends React.Component {
                         Kontrollera att labbmedlemsavtalet är signerat och säkerställ att rätt medlemsnummer står väl synligt på labbmedlemsavtalet.
                     </div>
                     <fieldset>
-                        <input className="uk-checkbox" type="checkbox"/>
+                        <input className="uk-checkbox" type="checkbox" checked={has_signed} onChange={(e) => this.signedChanged(e.target.checked)}/>
                         <label> Signerat labbmedlemsavtal mottaget.</label> 
                     </fieldset>
 
@@ -71,45 +109,19 @@ class KeyHandoutForm extends React.Component {
 
                     <fieldset>
                         <TextInput model={member} name="civicregno" title="Personnummer" />
+                        <div className="uk-container">
+                            <button className="uk-button uk-button-success uk-float-right" tabIndex="3" disabled={!can_save_member}><i className="uk-icon-save"/> {member.id ? 'Spara' : 'Skapa'}</button>
+                        </div>
                     </fieldset>
-                </div>
-
-                <div className="uk-section">
-                    <div className="uk-container">
-                        <h2>3. Spara information</h2>
-                        Spara informationen till databasen 		
-                    </div>
-                    <div className="uk-container">	
-                        <button className="uk-button uk-button-success uk-float-left" tabIndex="3" disabled={!can_save_member}><i className="uk-icon-save"/> {member.id ? 'Spara' : 'Skapa'}</button>
-                    </div>
                 </div>
             </form>
 
             <form className="uk-form" onSubmit={(e) => {e.preventDefault(); this.createKey(); return false;}}>
                 <div className="uk-section">
                     <div className="uk-container">
-                        <h2>4. Konfigurera nyckel </h2>
-                        Läs av RFID-taggens unika nummer med hjälp av läsaren. 		
+                        <h2>3. Kontrollera nyckel </h2>		
                     </div>
-
-                <div className="uk-section"></div>
-                    <div className="uk-width-1-1">
-                        <TextInput model={this.key} tabIndex="1" name="tagid" title="RFID" placeholder="Använd en RFID-läsare för att läsa av det unika numret på nyckeln" />
-                        <TextInput model={this.key} tabIndex="2" name="description" title="Kommentar" placeholder="Det är valfritt att lägga in en kommentar av nyckeln" />
-
-                        <div className="uk-form-row uk-margin-top">
-                            <div className="uk-form-controls">
-                                <button className="uk-button uk-button-success uk-float-right" disabled={!can_save_key}><i className="uk-icon-save"/>Skapa nyckel</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <CollectionTable emptyMessage="Medlemmen har ingen nyckel" rowComponent={Row(this.keyCollection, member.id)} collection={this.keyCollection} columns={[
-                        {title: "RFID", sort: "tagid"},
-                        {title: "Kommentar"},
-                        {title: "Skapad", sort: "created_at"},
-                        {title: ""},
-                    ]} />
+                    {key_paragraph}
                 </div>
             </form>
         </div>
