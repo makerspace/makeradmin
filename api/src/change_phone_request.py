697d8f7e0c57ca1667d714634fd050b0aee216c6
Anders Roos <anders.roos@gmail.com>
2022-06-06 20:18:29 +0200

    Prefent spamming of guessing code.

--- api/src/change_phone_request.py
+++ api/src/change_phone_request.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from datetime import timedelta, datetime
 from random import randint
 import logging
@@ -13,7 +14,6 @@ logger = logging.getLogger('makeradmin')
 
 
 def change_phone_request(member_id, phone):
-    logging.info(f'member {member_id} channge phone number request, phone {phone}')
     now = datetime.utcnow()
 
     total_requests = db_session.query(PhoneNumberChangeRequest).filter(PhoneNumberChangeRequest.timestamp >= now-timedelta(days=30)).count()
@@ -51,12 +51,22 @@ def change_phone_validate(member_id, validation_code):
         change_request = db_session.query(PhoneNumberChangeRequest).filter(
             PhoneNumberChangeRequest.member_id == member_id,
         ).order_by(desc(PhoneNumberChangeRequest.timestamp)).first()
+
+        if inc_tries(member_id) > 1000:
+            logging.info(f'member {member_id} validating phone number, too many tries, aborting')
+            raise BadRequest("Du har gissat för många gånger, block!")
+        
+        if change_request.completed:
+            logging.info(f'member {member_id} validating phone number, code already completed, code {validation_code}')
+            raise BadRequest("Koden är redan använd")
+
         if change_request.timestamp <= now-timedelta(minutes=5):
             logging.info(f'member {member_id} validating phone number, too old request, code {validation_code}')
             raise BadRequest("Koden är gammal")
         
         if change_request.validation_code == validation_code:
             change_request.member.phone = change_request.phone
+            change_request.completed = True
             db_session.flush()
             logging.info(f'member {member_id} validating phone number, success, changed phone number to {change_request.phone}')
             return {}
@@ -66,3 +76,13 @@ def change_phone_validate(member_id, validation_code):
     
     logging.info(f'member {member_id} validating phone number, no request for this member')
     raise NotFound("Koden är fel")
+
+
+validation_tries = defaultdict(lambda: 0)
+
+
+def inc_tries(member_id):
+    validation_tries[member_id] += 1
+    return validation_tries[member_id]
+
+
