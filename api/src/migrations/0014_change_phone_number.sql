f4a4c9350f5643fb2a471dec470e0a92df4ad7c3
Anders Roos <anders.roos@gmail.com>
2022-06-28 21:55:03 +0200

    Remove all existing phone numbers in migration.

--- api/src/migrations/0014_change_phone_number.sql
+++ api/src/migrations/0014_change_phone_number.sql
@@ -10,3 +10,7 @@ CREATE TABLE IF NOT EXISTS `change_phone_number_requests` (
   KEY `change_phone_number_member_id_index` (`member_id`),
   CONSTRAINT `change_phone_number_member_id_foreign` FOREIGN KEY (`member_id`) REFERENCES `membership_members` (`member_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--- comment out after migrations is done:
+UPDATE membership_members SET phone = NULL;
+
