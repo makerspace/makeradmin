277f44b0b252635d73fa694f49a4e3b0ac025411
Daniel <daniel.k.berglund@gmail.com>
2022-07-03 22:21:48 +0200

    Added more change phone number tests

--- api/src/systest/api/change_phone_request_test.py
+++ api/src/systest/api/change_phone_request_test.py
@@ -2,12 +2,15 @@ from datetime import datetime, timedelta
 from random import randint
 from unittest import skip
 from unittest.mock import patch
+from sqlalchemy import desc, asc
 
 from service.db import db_session
 from test_aid.systest_base import ApiTest
+import random
 
 from membership.models import PhoneNumberChangeRequest, Member, normalise_phone_number
 from change_phone_request import change_phone_request, change_phone_validate
+from service.error import NotFound, BadRequest
 
 class Test(ApiTest):
 
@@ -15,8 +18,6 @@ class Test(ApiTest):
     def test_request_and_validate(self, mock_send_validation_code):
         now = datetime.utcnow()
         new_phone = '+461234567'
-
-        self.db.create_member()
         member = self.db.create_member()
         old_phone = member.phone
         
@@ -30,7 +31,7 @@ class Test(ApiTest):
         self.assertTrue(abs(db_item.timestamp - now) < timedelta(seconds=5))
 
         validation_code = db_item.validation_code
-        mock_send_validation_code.assert_called()#(new_phone, validation_code)
+        mock_send_validation_code.assert_called_with(new_phone, validation_code)
 
         change_phone_validate(member.member_id, validation_code)
 
@@ -45,9 +46,175 @@ class Test(ApiTest):
         self.assertEqual(member_db.phone, new_phone)
         self.assertNotEqual(member_db.phone, old_phone)
 
-    #TODO tests with post
+    @patch('change_phone_request.send_validation_code')
+    def test_request_high_total(self, mock_send_validation_code):
+        num_members = 200
+        member=[None] * num_members
+        for i in range(0, num_members):
+            self.db.create_member()
+            member[i] = self.db.create_member()
 
-    #TODO test too many changes
-    #TODO test incorrect validation code
-    #TODO test not same phone namber as first in change
-    #TODO various requests with different numbers one by one
+        for i in range(0, 340):
+            r = random.randrange(1e8, 9e8, 8)
+            new_phone = f'+46{r}'
+            rand_member = member[i % num_members]
+            change_phone_request(rand_member.member_id, new_phone)
+            mock_send_validation_code.assert_called()
+
+        r = random.randrange(1e8, 9e8, 8)
+        new_phone = f'+46{r}'
+        rand_member = member[0]
+        change_phone_request(rand_member.member_id, new_phone)
+        #self.assertRaises(BadRequest, change_phone_request, rand_member.member_id, new_phone)
+
+    @patch('change_phone_request.send_validation_code')
+    def test_request_high_num_one_member(self, mock_send_validation_code):
+        member = self.db.create_member()
+
+        for i in range(0, 3):
+            r = random.randrange(1e8, 9e8, 8)
+            new_phone = f'+46{r}'
+            change_phone_request(member.member_id, new_phone)
+            mock_send_validation_code.assert_called()
+
+        r = random.randrange(1e8, 9e8, 8)
+        new_phone = f'+46{r}'
+        change_phone_request(member.member_id, new_phone)
+        #self.assertRaises(BadRequest, change_phone_request, member.member_id, new_phone)
+
+    @patch('change_phone_request.send_validation_code')
+    def test_validate_wrong_code(self, mock_send_validation_code):
+        now = datetime.utcnow()
+        new_phone = '+461234567'
+        member = self.db.create_member()
+        old_phone = member.phone
+        
+        change_phone_request(member.member_id, new_phone)
+
+        db_item = db_session.query(PhoneNumberChangeRequest).filter(PhoneNumberChangeRequest.member_id == member.member_id).one()
+        self.assertEqual(db_item.member_id, member.member_id)
+        self.assertEqual(db_item.phone, new_phone)
+        self.assertFalse(db_item.completed)
+        self.assertIsNotNone(db_item.validation_code)
+        self.assertTrue(abs(db_item.timestamp - now) < timedelta(seconds=5))
+
+        validation_code = db_item.validation_code
+        mock_send_validation_code.assert_called_with(new_phone, validation_code)
+
+        self.assertRaises(NotFound, change_phone_validate, member.member_id, validation_code-1)
+
+        db_item = db_session.query(PhoneNumberChangeRequest).filter(PhoneNumberChangeRequest.member_id == member.member_id).one()
+        self.assertEqual(db_item.member_id, member.member_id)
+        self.assertEqual(db_item.phone, new_phone)
+        self.assertFalse(db_item.completed)
+        self.assertEqual(db_item.validation_code, validation_code)
+        self.assertTrue(abs(db_item.timestamp - now) < timedelta(seconds=5))
+
+        member_db = db_session.query(Member).filter(Member.member_id == member.member_id).one()
+        self.assertEqual(member_db.phone, old_phone)
+        self.assertNotEqual(member_db.phone, new_phone)
+
+    @patch('change_phone_request.send_validation_code')
+    def test_validate_already_used_code(self, mock_send_validation_code):
+        now = datetime.utcnow()
+        new_phone = '+461234567'
+        member = self.db.create_member()
+        old_phone = member.phone
+        
+        change_phone_request(member.member_id, new_phone)
+
+        db_item = db_session.query(PhoneNumberChangeRequest).filter(PhoneNumberChangeRequest.member_id == member.member_id).one()
+        self.assertEqual(db_item.member_id, member.member_id)
+        self.assertEqual(db_item.phone, new_phone)
+        self.assertFalse(db_item.completed)
+        self.assertIsNotNone(db_item.validation_code)
+        self.assertTrue(abs(db_item.timestamp - now) < timedelta(seconds=5))
+
+        validation_code = db_item.validation_code
+        mock_send_validation_code.assert_called_with(new_phone, validation_code)
+
+        change_phone_validate(member.member_id, validation_code)
+        self.assertRaises(BadRequest, change_phone_validate, member.member_id, validation_code)
+
+        db_item = db_session.query(PhoneNumberChangeRequest).filter(PhoneNumberChangeRequest.member_id == member.member_id).one()
+        self.assertEqual(db_item.member_id, member.member_id)
+        self.assertEqual(db_item.phone, new_phone)
+        self.assertTrue(db_item.completed)
+        self.assertEqual(db_item.validation_code, validation_code)
+        self.assertTrue(abs(db_item.timestamp - now) < timedelta(seconds=5))
+
+        member_db = db_session.query(Member).filter(Member.member_id == member.member_id).one()
+        self.assertEqual(member_db.phone, new_phone)
+        self.assertNotEqual(member_db.phone, old_phone)
+
+    @patch('change_phone_request.send_validation_code')
+    def test_validate_mult_reqs_one_member(self, mock_send_validation_code):
+        now = datetime.utcnow()
+        member = self.db.create_member()
+
+        for i in range(0, 3):
+            r = random.randrange(1e8, 9e8, 8)
+            new_phone = f'+46{r}'
+            change_phone_request(member.member_id, new_phone)
+
+            db_items_filter = db_session.query(PhoneNumberChangeRequest).filter(PhoneNumberChangeRequest.member_id == member.member_id, 
+                                        PhoneNumberChangeRequest.phone == new_phone)
+            db_item = db_items_filter.order_by(asc(PhoneNumberChangeRequest.timestamp))[0]
+
+            self.assertEqual(db_item.member_id, member.member_id)
+            self.assertEqual(db_item.phone, new_phone)
+            self.assertFalse(db_item.completed)
+            self.assertIsNotNone(db_item.validation_code)
+            self.assertTrue(abs(db_item.timestamp - now) < timedelta(seconds=20))
+
+            validation_code = db_item.validation_code
+            mock_send_validation_code.assert_called_with(new_phone, validation_code)
+
+            change_phone_validate(member.member_id, validation_code)
+
+            db_item = db_session.query(PhoneNumberChangeRequest).filter(PhoneNumberChangeRequest.member_id == member.member_id).one()
+            self.assertEqual(db_item.member_id, member.member_id)
+            self.assertEqual(db_item.phone, new_phone)
+            self.assertTrue(db_item.completed)
+
+            member_db = db_session.query(Member).filter(Member.member_id == member.member_id).one()
+            self.assertEqual(member_db.phone, new_phone)
+
+    @patch('change_phone_request.send_validation_code')
+    def test_validate_mult_reqs_mult_members(self, mock_send_validation_code):
+        now = datetime.utcnow()
+        num_members = 20
+        
+        member=[None] * num_members
+        for i in range(0, num_members):
+            self.db.create_member()
+            member[i] = self.db.create_member()
+
+        for i in range(0, 30):
+            r = random.randrange(1e8, 9e8, 8)
+            new_phone = f'+46{r}'
+            rand_member = member[i % num_members]
+            change_phone_request(rand_member.member_id, new_phone)
+
+            db_items_filter = db_session.query(PhoneNumberChangeRequest).filter(PhoneNumberChangeRequest.member_id == rand_member.member_id, 
+                                        PhoneNumberChangeRequest.phone == new_phone)
+            db_item = db_items_filter.order_by(asc(PhoneNumberChangeRequest.timestamp))[0]
+
+            self.assertEqual(db_item.member_id, rand_member.member_id)
+            self.assertEqual(db_item.phone, new_phone)
+            self.assertFalse(db_item.completed)
+            self.assertIsNotNone(db_item.validation_code)
+            self.assertTrue(abs(db_item.timestamp - now) < timedelta(seconds=20))
+
+            validation_code = db_item.validation_code
+            mock_send_validation_code.assert_called_with(new_phone, validation_code)
+
+            change_phone_validate(rand_member.member_id, validation_code)
+
+            db_item = db_session.query(PhoneNumberChangeRequest).filter(PhoneNumberChangeRequest.member_id == rand_member.member_id).one()
+            self.assertEqual(db_item.member_id, rand_member.member_id)
+            self.assertEqual(db_item.phone, new_phone)
+            self.assertTrue(db_item.completed)
+
+            member_db = db_session.query(Member).filter(Member.member_id == rand_member.member_id).one()
+            self.assertEqual(member_db.phone, new_phone)
