46be2a3cc26db204ae1c5d19e028e16ed34034d4
Daniel <daniel.k.berglund@gmail.com>
2022-07-17 15:06:45 +0200

    Add migration for group tags

---
+++ api/src/migrations/0014_user_tags.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `membership_tags` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `tag` varchar(255) UNIQUE COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tag_group_id_index` (`group_id`),
+  CONSTRAINT `tag_group_id_foreign` FOREIGN KEY (`group_id`) REFERENCES `membership_groups` (`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
