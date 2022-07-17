4797b4b9c77147ae6bcaf7d6ad029371b59f040f
Daniel <daniel.k.berglund@gmail.com>
2022-07-17 16:01:21 +0200

    Fix sql

--- api/src/migrations/0014_user_tags.sql
+++ api/src/migrations/0014_user_tags.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS `membership_tags` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `group_id` int(10) unsigned NOT NULL,
   `tag` varchar(255) UNIQUE COLLATE utf8mb4_0900_ai_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `tag_group_id_index` (`group_id`),
