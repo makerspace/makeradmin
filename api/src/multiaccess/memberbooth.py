7f613b3345c22f5ce8b3748c7d5f48d0ac72e782
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2023-01-08 12:51:33 +0100

    Remove util.py since it only had one function used in one place

--- api/src/multiaccess/memberbooth.py
+++ api/src/multiaccess/memberbooth.py
@@ -4,10 +4,20 @@ from membership.membership import get_membership_summary
 from service.db import db_session
 from service.error import NotFound
 from membership.models import Member, Key
-from .util import member_to_response_object
 
 
 logger = getLogger("makeradmin")
+
+
+def member_to_response_object(member: Member):
+    return {
+        'member_id': member.member_id,
+        'member_number': member.member_number,
+        'firstname': member.firstname,
+        'lastname': member.lastname,
+        'end_date': max((span.enddate for span in member.spans)).isoformat() if len(member.spans) > 0 else None,
+        'keys': [{'key_id': key.key_id, 'rfid_tag': key.tagid} for key in member.keys],
+    }
 
 
 def memberbooth_response_object(member: Member, membership_data):
