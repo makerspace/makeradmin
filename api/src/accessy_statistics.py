09e01f34da0b1bb371fbff0166ad371556bb8390
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2023-01-25 23:10:39 +0100

    Only run if main module

--- api/src/accessy_statistics.py
+++ api/src/accessy_statistics.py
@@ -6,25 +6,25 @@ from multiaccessy.unlock_statistics import ensure_init_db, ensure_door_exists, i
 from service.config import config
 
 
-con = sqlite3.connect("accessy.db")
-cur = con.cursor()
+def main():
+    con = sqlite3.connect("accessy.db")
+    ensure_init_db(con)
 
+    ACCESSY_SESSION_TOKEN = config.get("ACCESSY_SESSION_TOKEN")
+    accessy_session.session_token = ACCESSY_SESSION_TOKEN
+    accessy_session.session_token_token_expires_at = datetime.now() + timedelta(days=1)
 
-ensure_init_db(con)
+    doors = accessy_session.get_all_doors()
+    for door in doors:
+        ensure_door_exists(con, door)
 
-ACCESSY_SESSION_TOKEN = config.get("ACCESSY_SESSION_TOKEN")
-accessy_session.session_token = ACCESSY_SESSION_TOKEN
-accessy_session.session_token_token_expires_at = datetime.now() + timedelta(days=1)
+    accesses: list[Access] = []
+    for door in doors:
+        print(f"Getting unlocks for door {door.name!r}")
+        accesses = accessy_session.get_all_accesses(door)
+        row_updates = insert_accesses(con, accesses)
+        print(f"    {row_updates} new unlocks")
+    
 
-
-doors = accessy_session.get_all_doors()
-
-for door in doors:
-    ensure_door_exists(con, door)
-
-accesses: list[Access] = []
-for door in doors:
-    print(f"Getting unlocks for door {door.name!r}")
-    accesses = accessy_session.get_all_accesses(door)
-    row_updates = insert_accesses(con, accesses)
-    print(f"    {row_updates} new unlocks")
+if __name__ == "__main__":
+    main()
