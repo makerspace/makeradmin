e2f513d1af2148c5af21e4a7b68b87aa577930c7
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2023-01-25 23:41:22 +0100

    Break out fetch into own function; prepare for more functions

--- api/src/accessy_statistics.py
+++ api/src/accessy_statistics.py
@@ -1,3 +1,5 @@
+from argparse import ArgumentParser
+from enum import Enum
 from datetime import datetime, timedelta
 import sqlite3
 
@@ -6,11 +8,11 @@ from multiaccessy.unlock_statistics import ensure_init_db, ensure_door_exists, i
 from service.config import config
 
 
-def main():
-    con = sqlite3.connect("accessy.db")
+def fetch_accessy(con: sqlite3.Connection):
     ensure_init_db(con)
 
     ACCESSY_SESSION_TOKEN = config.get("ACCESSY_SESSION_TOKEN")
+    # Use cached token if it exists
     accessy_session.session_token = ACCESSY_SESSION_TOKEN
     accessy_session.session_token_token_expires_at = datetime.now() + timedelta(days=1)
 
@@ -24,6 +26,27 @@ def main():
         accesses = accessy_session.get_all_accesses(door)
         row_updates = insert_accesses(con, accesses)
         print(f"    {row_updates} new unlocks")
+
+
+class Modes(Enum):
+    fetch = "fetch"
+    
+    def __repr__(self):
+        return self.name
+
+
+def main():
+    parser = ArgumentParser("Fetch data about door unlocks from Accessy and store in local database")
+    parser.add_argument("--database", default="accessy.db", help="Name of sqlite3 database to use")
+    parser.add_argument("--mode", type=Modes, choices=list(Modes), default=Modes.fetch, help="Mode to run program in")
+    args = parser.parse_args()
+
+    con = sqlite3.connect(args.database)
+
+    if args.mode == Modes.fetch:
+        fetch_accessy(con)
+    else:
+        raise RuntimeError(f"Unhandled mode {args.mode}")
     
 
 if __name__ == "__main__":
