0e5da7f74e19a7e86c2fdc9dff89fd696aad26a3
Aron Granberg <aron.granberg@gmail.com>
2023-03-14 21:12:05 +0100

    Ensure `make firstrun` actually works. And update docker to use `docker compose`.

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-docker-compose exec db2 bash -c "mysql --default-character-set=utf8mb4 -uroot -p\${MYSQL_ROOT_PASSWORD} makeradmin"
+docker compose exec db2 bash -c "mysql --default-character-set=utf8mb4 -uroot -p\${MYSQL_ROOT_PASSWORD} makeradmin"
