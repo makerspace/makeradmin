fd3091c2c79c2254c23d634276cd409afe6812df
Aron Granberg <aron.granberg@gmail.com>
2023-03-28 11:53:42 +0200

    Add db restore script.

---
+++ db_restore.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+file_path="$1"
+
+if [ -z "$file_path" ]; then
+    echo "Usage: db_restore.sh <path to sql file>"
+    exit 1
+fi
+
+if [ ! -f "$file_path" ]; then
+    echo "File not found: $file_path"
+    exit 1
+fi
+
+# Prompt for confirmation before restoring
+#read -p "This will delete your current database!! Are you sure you want to continue? (y/n) " -n 1 -r
+while true; do
+    read -p "This will delete your current database!! Are you sure you want to continue? " yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo ""
+echo "Restoring DB..."
+
+cat $file_path | docker compose exec -T db2 bash -c "exec mysql --default-character-set=utf8mb4 -uroot -p\${MYSQL_ROOT_PASSWORD} makeradmin"
+
+echo "Done"
