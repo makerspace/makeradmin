c10d977101c1855732cbd87b4e906d480a181d0b
Aron Granberg <aron.granberg@gmail.com>
2023-06-12 11:22:40 +0200

    Make restart.sh also restart reverse proxy, as it otherwise sometimes fails.

--- restart.sh
+++ restart.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 docker compose build && docker compose -f docker-compose.yml -f docker-compose.prod.yml  down --remove-orphans && ./start.sh
 
+echo "sleeping a long long time then restarting the reverse proxy to make certs work again"
+sleep 12
+docker restart reverse-proxy
