21d3c0cc93affebfac79ee8dd2f4745751f56fa1
Aron Granberg <aron.granberg@gmail.com>
2023-06-18 20:18:56 +0200

    Move URLs to separate TS file.

---
+++ public/ts/urls.ts
@@ -0,0 +1,6 @@
+export const FACEBOOK_GROUP = "https://www.facebook.com/groups/makerspace.se"
+export const SLACK_HELP = "https://wiki.makerspace.se/Slack"
+export const WIKI = "https://wiki.makerspace.se"
+export const GET_STARTED_QUIZ = "https://medlem.makerspace.se/member/quiz/1"
+export const INSTAGRAM = "https://www.instagram.com/stockholmmakerspace/"
+export const RELATIVE_MEMBER_PORTAL = "/member";
