506d687a40ffac66fe034236f86166ebe87bc174
Aron Granberg <aron.granberg@gmail.com>
2023-06-18 20:11:01 +0200

    Change member creation to use speculative transactions instead of manual locking.

--- api/src/membership/member_entity.py
+++ api/src/membership/member_entity.py
@@ -18,6 +18,8 @@ def handle_password(data):
     if unhashed_password is not None:
         data['password'] = check_and_hash_password(unhashed_password)
 
+class RaceCondition(Exception):
+    pass
 
 class MemberEntity(Entity):
     """
@@ -35,24 +37,24 @@ class MemberEntity(Entity):
 
         handle_password(data)
 
-        status, = db_session.execute("SELECT GET_LOCK('member_number', 20)").fetchone()
-        if not status:
-            raise InternalServerError("Failed to create member, try again later.",
-                                      log="failed to aquire member_number lock")
-        try:
-            if data.get('member_number') is None:
-                sql = "SELECT COALESCE(MAX(member_number), 999) FROM membership_members"
-                max_member_number, = db_session.execute(sql).fetchone()
-                data['member_number'] = max_member_number + 1
-            obj = self.to_obj(self._create_internal(data, commit=commit))
-            logger.info(f"created member with number {data['member_number']}")
-            return obj
-        except Exception:
-            # Rollback session if anything went wrong or we can't release the lock.
-            db_session.rollback()
-            raise
-        finally:
-            db_session.execute("DO RELEASE_LOCK('member_number')")
+        assert 'member_number' not in data
+        # Locking was used here previously, but that did not work well with transactions
+        # so now we use transactions to solve EVERYTHING.
+        # In practice we will never get a race here, except possibly in tests that run in parallel.
+        while True:
+            try:
+                with db_session.begin_nested():
+                    max_member_number, = db_session.execute("SELECT COALESCE(MAX(member_number), 999) FROM membership_members").fetchone()
+                    data['member_number'] = max_member_number + 1
+                    obj = self.to_obj(self._create_internal(data, commit=commit))
+                    logger.info(f"created member with number {data['member_number']}")
+                    if db_session.query(self.model).filter(Member.member_number == data['member_number']).count() != 1:
+                        raise RaceCondition()
+                    return obj
+            except RaceCondition as e:
+                print("Race condition when creating member, retrying...")
+                # Try again
+                continue
 
     def update(self, entity_id: int, commit=True):
         data = request.json or {}
