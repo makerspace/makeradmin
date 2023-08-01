1cb81a50c727f96525d56184647eaa5e816e5586
Aron Granberg <aron.granberg@gmail.com>
2023-08-01 10:26:26 +0200

    Convert the cart page to pure jsx.

--- public/ts/cartpage.tsx
+++ public/ts/cartpage.tsx
@@ -1,108 +1,73 @@
-import Cart from "./cart"
+import Cart, { Item, useCart } from "./cart"
 import * as common from "./common"
+import * as login from "./login"
 import { ServerResponse, UNAUTHORIZED } from "./common";
 import { renderSidebarCategories } from "./category"
-import { BackendPaymentResponse, PaymentFlowDefinition, mountStripe, pay } from "./payment_common"
+import { BackendPaymentResponse, LoadProductData, PaymentFlowDefinition, Product, ProductData, StripeCardInput, createStripeCardInput, initializeStripe, mountStripe, negotiatePayment, pay } from "./payment_common"
+import { LoadCurrentMemberInfo } from "./member_common";
+import { render } from "preact";
+import { stripe } from './payment_common';
+import { useMemo, useState } from "preact/hooks";
+import { Sidebar } from "./sidebar";
 declare var UIkit: any;
 
-common.onGetAndDocumentLoaded("/webshop/product_data", (productData: any) => {
-	common.addSidebarListeners();
-
-	renderSidebarCategories(productData);
-
-	const id2item = new Map<number, any>();
-
-	for (const category of productData) {
-		for (const item of category.items) {
-			id2item.set(item.id, item);
-		}
+const CartItem = ({ cartItem, productData, cart, onChangeCart }: { cartItem: Item, productData: ProductData, cart: Cart, onChangeCart: (cart: Cart) => void }) => {
+	const item = productData.id2item.get(cartItem.id);
+	if (item === undefined) {
+		// Item no longer exists
+		cart.setItem(cartItem.id, 0);
+		onChangeCart(cart);
+		return null;
 	}
 
-	function setCartItem(productID: number, count: number) {
-		let cart = Cart.fromStorage();
-		cart.setItem(productID, count);
-		cart.saveToStorage();
-		common.updateCartSum(cart, id2item);
-	}
+	let price = Number(item.price) * item.smallest_multiple;
 
-	function createElements(cart: Cart) {
-		common.updateCartSum(cart, id2item);
-		document.querySelector("#cart")!.innerHTML = "";
+	let baseStr = item.smallest_multiple > 1 ? item.smallest_multiple + item.unit : item.unit;
 
-		if (cart.items.length == 0) {
-			document.querySelector("#cart")!.innerHTML = "<p class='empty-cart-text'>Du har inga produkter i varukorgen.</p>";
-			(<HTMLButtonElement>document.querySelector("#pay-button")).disabled = true;
-			return;
-		}
+	return <div class="product">
+		<div class="product-line">
+			<a class="product-title" href="product/${item.id}">{item.name}</a>
+			<span class="product-price">{price} {Cart.currency}/{baseStr}</span>
+		</div>
+		<div class="product-input product-line">
+			<input type="number" min={0} max={9999} value={cartItem.count} step={item.smallest_multiple} placeholder="0" class="product-amount"
+				onChange={e => {
+					let newAmount = Cart.adjustItemCount(Number(e.currentTarget.value), item);
+					cart.setItem(item.id, newAmount);
+					onChangeCart(cart);
+				}}
+			/>
+			<span class="product-unit">{item.unit}</span>
+			<button type="button" class="product-delete uk-button uk-button-small uk-button-danger" uk-icon="trash"
+				onClick={e => {
+					cart.setItem(item.id, 0);
+					onChangeCart(cart);
+				}}
+			/>
+		</div>
+	</div>;
+}
 
-		(<HTMLButtonElement>document.querySelector("#pay-button")).disabled = false;
+type PaymentRequest = {
+	cart: Item[],
+	expected_sum: number,
+	stripe_payment_method_id: string,
+	transaction_id: string | null
+}
 
-		for (const cartItem of cart.items) {
-			const item = id2item.get(cartItem.id);
-			let price = item.price;
-			price *= item.smallest_multiple;
-
-			let baseStr = item.smallest_multiple > 1 ? item.smallest_multiple + item.unit : item.unit;
-
-			const li = document.createElement("li");
-			li.innerHTML = `
-				<div class="product">
-					<div class="product-line">
-						<a class="product-title" href="product/${item.id}">${item.name}</a>
-						<span class="product-price">${price} ${Cart.currency}/${baseStr}</span>
-					</div>
-					<div class="product-input product-line">
-						<input type="number" min=0 max=9999 step=${item.smallest_multiple} placeholder="0" class="product-amount"></input>
-						<span class="product-unit">${item.unit}</span>
-						<button type="button" class="product-delete uk-button uk-button-small uk-button-danger" uk-icon="trash" ></button>
-					</div>
-				</div>`;
-			id2item.set(item.id, item);
-
-			// Handle deleting products
-			li.querySelector(".product-delete")!.addEventListener("click", ev => {
-				ev.preventDefault();
-				setCartItem(item.id, 0);
-				li.remove();
-			});
-
-			const productAmount = <HTMLInputElement>li.querySelector(".product-amount");
-			// Select everything in the textfield when clicking on it
-			productAmount.onclick = () => {
-				productAmount.select();
-			};
-
-			productAmount.oninput = ev => {
-				let newAmount = Cart.adjustItemCount(Number(productAmount.value), item);
-				setCartItem(item.id, newAmount);
-			};
-
-			productAmount.onchange = ev => {
-				let newAmount = Cart.adjustItemCount(Number(productAmount.value), item);
-				setCartItem(item.id, newAmount);
-				productAmount.value = "" + newAmount;
-			};
-
-			productAmount.value = "" + cartItem.count;
-			document.querySelector("#cart")!.appendChild(li);
-		}
-	}
-
-	mountStripe();
-
+const PaymentButton = ({ cart, productData }: { cart: Cart, productData: ProductData }) => {
 	function pay_config(): PaymentFlowDefinition {
 		return {
 			initiate_payment: (result) => {
-				let cart = Cart.fromStorage();
 				return common.ajax("POST", window.apiBasePath + "/webshop/pay", {
 					cart: cart.items,
-					expected_sum: cart.sum(id2item),
+					expected_sum: cart.sum(productData.id2item),
 					stripe_payment_method_id: result.paymentMethod.id,
 				})
 			},
 			on_payment_success: (json: ServerResponse<BackendPaymentResponse>) => {
-				window.location.href = "receipt/" + json.data.transaction_id;
 				new Cart([]).saveToStorage();
+				window.location.href = "receipt/" + json.data.transaction_id;
 			},
 			on_failure: (json: ServerResponse<any>) => {
 				if (json.status === UNAUTHORIZED) {
@@ -113,42 +78,101 @@ common.onGetAndDocumentLoaded("/webshop/product_data", (productData: any) => {
 			},
 		};
 	}
-	const payment_config = pay_config();
 
-	function login() {
-		common.ajax("POST", window.apiBasePath + "/member/send_access_token", {
-			user_identification: (<HTMLInputElement>document.querySelector("#email")).value,
-			redirect: "/shop/cart"
-		}).then(json => {
-			UIkit.modal.alert("En inloggningslänk har skickats via email");
-		}).catch(json => {
-			if (json.status == "not found") {
-				UIkit.modal.alert("Ingen medlem med denna email hittades");
+	const [inProgress, setInProgress] = useState(false);
+	const paymentConfig = pay_config();
+	const element = useMemo(() => createStripeCardInput(), []);
+
+	return <>
+		<form id="pay">
+			<h3>Betala med kort via Stripe</h3>
+			<div class="form-row">
+				<div id="payment_element"></div>
+				<div id="card-element">
+					<StripeCardInput element={element} />
+				</div>
+				<div id="card-errors" role="alert"></div>
+			</div>
+
+			<button type="button" id="pay-button" class="spinner-button uk-button uk-button-default uk-button-primary" disabled={inProgress} onClick={async e => {
+				e.preventDefault();
+				setInProgress(true);
+				try {
+					const result = await stripe.createPaymentMethod('card', element);
+					if (result.error) {
+						throw result.error;
+					} else {
+						const payment = await negotiatePayment<PaymentRequest, BackendPaymentResponse>(window.apiBasePath + "/webshop/pay", {
+							cart: cart.items,
+							expected_sum: cart.sum(productData.id2item),
+							stripe_payment_method_id: result.paymentMethod!.id,
+							transaction_id: null,
+						})
+
+						// Payment succeeded! Clear the cart and go to the receipt page.
+						new Cart([]).saveToStorage();
+						window.location.href = "receipt/" + payment.transaction_id;
+					}
+				} catch (e) {
+					common.show_error("Betalningen misslyckades", e);
+				} finally {
+					setInProgress(false);
+				}
+			}}>
+				<span class={`uk-spinner uk-icon progress-spinner ${inProgress ? 'progress-spinner-visible' : ''}`} uk-spinner={''}></span>
+				<span>Betala / Pay<span> {Cart.formatCurrency(cart.sum(productData.id2item))}</span></span>
+			</button>
+		</form>
+	</>
+}
+
+
+const CartPage = ({ productData }: { productData: ProductData }) => {
+	const { cart, setCart } = useCart();
+
+	return <>
+		<Sidebar cart={{ cart, productData }} />
+		<div id="content" class="cartpage">
+			<div class="content-centering">
+				<h3 class="cart-header">Varukorg</h3>
+				<ul id="cart" class="layout-table">
+					{cart.items.length > 0 ?
+						cart.items.map(cartItem => <CartItem cartItem={cartItem} productData={productData} cart={cart} onChangeCart={cart => {
+							cart.saveToStorage();
+							setCart(Cart.fromStorage());
+						}} />)
+						: <p class='empty-cart-text'>Du har inga produkter i varukorgen.</p>}
+				</ul>
+				<div id="pay-module">
+					{cart.items.length > 0 && <PaymentButton cart={cart} productData={productData} />}
+				</div>
+			</div>
+		</div>
+	</>;
+}
+
+common.documentLoaded().then(() => {
+	const productData = LoadProductData();
+	const member = LoadCurrentMemberInfo();
+	const root = document.querySelector("#root") as HTMLElement;
+	initializeStripe();
+
+	Promise.all([member, productData]).then(([member, productData]) => {
+		if (root != null) {
+			root.innerHTML = "";
+			render(
+				<CartPage productData={productData} />,
+				root
+			);
+		}
+	})
+		.catch(json => {
+			// Probably Unauthorized, redirect to login page.
+			if (json.status === common.UNAUTHORIZED) {
+				// Render login
+				login.render_login(root, null, null);
 			} else {
-				UIkit.modal.alert("<h2>Error</h2>" + common.get_error(json));
+				UIkit.modal.alert("<h2>Misslyckades med att hämta köphistorik</h2>" + common.get_error(json));
 			}
 		});
-	}
-
-	function setLoggedIn(loggedIn: boolean) {
-		document.querySelector("#pay-login")!.classList.toggle("active", !loggedIn);
-		document.querySelector("#pay")!.classList.toggle("active", loggedIn);
-	}
-
-	common.refreshLoggedIn((loggedIn, permissions) => {
-		setLoggedIn(loggedIn);
-	});
-
-	Cart.startLocalStorageSync(createElements);
-	createElements(Cart.fromStorage());
-
-	document.querySelector("#pay-button")!.addEventListener("click", ev => {
-		ev.preventDefault();
-		pay(payment_config);
-	});
-
-	document.querySelector("#pay-login form")!.addEventListener("submit", ev => {
-		ev.preventDefault();
-		login();
-	});
 });
