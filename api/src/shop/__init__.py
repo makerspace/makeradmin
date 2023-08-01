51c2c125c8b4747fcaa88d27ec53b45c040970a8
Aron Granberg <aron.granberg@gmail.com>
2023-08-01 16:39:32 +0200

    Don't set stripe api version twice.

--- api/src/shop/__init__.py
+++ api/src/shop/__init__.py
@@ -3,8 +3,7 @@ import stripe
 from service.internal_service import InternalService
 from service.config import config
 
-stripe.api_version = '2022-11-15'
-service = InternalService(name='shop')
+service = InternalService(name="shop")
 
 
 import shop.views
