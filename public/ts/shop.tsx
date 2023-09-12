519fd88fda23daa3ec18ea85a69bdb08ba45b525
Aron Granberg <aron.granberg@gmail.com>
2023-09-12 13:51:26 +0200

    Fix input field in shop rounding too aggressively. Fixes #315.

--- public/ts/shop.tsx
+++ public/ts/shop.tsx
@@ -14,13 +14,47 @@ const ProductCategoryHeader = ({ category }: { category: ProductCategory }) => {
   return <li><h3 id={`category${category.id}`}>{category.name}</h3></li>
 }
 
+/// Input field which only allows integers, and rounds up to the nearest step
+const RoundedInputField = ({ value, onChange, min, max, step, placeholder, className }: { value: number, onChange: (value: number) => void, min: number, max: number, step: number, placeholder: string, className?: string }) => {
+  // Show the placeholder if the value is zero
+  const valueStr = value != 0 ? `${value}` : "";
+
+  // Keep track of the current value in the textfield, but only show the rounded and clamped value when the user removes focus from the text field
+  const [tempValue, setTempValue] = useState(valueStr);
+  const [focused, setFocused] = useState(false);
+  if (!focused && tempValue !== valueStr) {
+    setTempValue(valueStr);
+  }
+
+  return <input type="text" pattern="[0-9]*" inputMode="numeric" step={step} placeholder={placeholder} value={tempValue} className={className} onClick={e => {
+    // Select everything in the textfield when clicking on it
+    e.currentTarget.select()
+  }}
+    onInput={e => {
+      const normalized = e.currentTarget.value.replace(",", ".");
+      setTempValue(normalized);
+      const num = Number(normalized);
+      if (normalized != "" && isFinite(num)) {
+        // Note: the |0 will ensure the number is a 32-bit integer
+        const rounded = (Math.ceil(num / step) * step) | 0;
+        const clamped = Math.min(Math.max(rounded, min), max);
+        onChange(clamped);
+      }
+    }}
+    onFocus={e => {
+      setFocused(true);
+    }}
+    onBlur={e => {
+      setFocused(false);
+    }}
+  />
+}
+
 const ProductItem = ({ item, cart, onChangeCart }: { item: Product, cart: Cart, onChangeCart: (cart: Cart) => void }) => {
   const price = Number(item.price) * item.smallest_multiple;
 
   let baseStr = item.smallest_multiple > 1 ? item.smallest_multiple + item.unit : item.unit;
 
-  const li = document.createElement("div");
-  li.className = "product-container"
   const image_url = `${window.apiBasePath}/webshop/image/${item.image_id || 0}`;
   const count = cart.getItemCount(item.id);
 
@@ -36,25 +70,22 @@ const ProductItem = ({ item, cart, onChangeCart }: { item: Product, cart: Cart, 
         <span class="product-price">{price} {Cart.currency}/{baseStr}</span>
       </div>
       <div class="product-input product-line">
-        <input type="number" min={0} max={9999} step={item.smallest_multiple} placeholder="0" value={count > 0 ? "" + count : ""} class="product-amount" onClick={e => {
-          // Select everything in the textfield when clicking on it
-          e.currentTarget.select()
-        }}
-          onInput={e => {
-            let newAmount = Cart.adjustItemCount(Number(e.currentTarget.value), item);
-            cart.setItem(item.id, newAmount);
-            onChangeCart(cart);
-          }}
-          onChange={e => {
-            let newAmount = Cart.adjustItemCount(Number(e.currentTarget.value), item);
-            cart.setItem(item.id, newAmount);
+        <RoundedInputField
+          min={0}
+          max={9999}
+          step={item.smallest_multiple}
+          placeholder="0"
+          value={count}
+          className="product-amount"
+          onChange={newAmount => {
+            cart.setItem(item.id, Cart.adjustItemCount(newAmount, item));
             onChangeCart(cart);
           }}
         />
         <span class="product-unit">{item.unit}</span>
         <button type="button" class="number-add uk-button uk-button-small uk-button-primary" data-amount="1"
           onClick={e => {
-            cart.setItem(item.id, cart.getItemCount(item.id) + 1);
+            cart.setItem(item.id, Cart.adjustItemCount(cart.getItemCount(item.id) + 1, item));
             onChangeCart(cart);
             e.preventDefault();
           }}
