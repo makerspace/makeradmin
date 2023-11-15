77a048c60d07776e952f4750241fa42a59fe4a4b
Daniel Berglund <daniel.k.berglund@gmail.com>
2023-11-15 23:26:16 +0100

    Fix stripe currency bug in dev environment

--- api/src/service/config.py
+++ api/src/service/config.py
@@ -37,6 +37,7 @@ default = Dict(
         HOST_PUBLIC="",
         STRIPE_PRIVATE_KEY=None,
         STRIPE_SIGNING_SECRET=None,
+        STRIPE_CURRENCY=None,
         APP_DEBUG=None,
         CORS_ALLOWED_ORIGINS="https://medlem.makerspace.se,https://stockholm.makeradmin.se,https://medlem.dev.makerspace.se"
         ",http://localhost:8009,http://localhost:8011,http://localhost:8080",
