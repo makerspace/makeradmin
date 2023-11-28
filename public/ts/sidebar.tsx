6663fa8999ae8ab94cede9316129d1fee13197df
Arman <arman.iqbal@prevas.se>
2023-11-28 11:11:02 +0100

    Sidebar should get vertical scrollbar when the amount of elements exceeds the window height.

--- public/ts/sidebar.tsx
+++ public/ts/sidebar.tsx
@@ -1,4 +1,5 @@
-import { ComponentChildren } from "preact";
+import { useEffect, useRef, useState } from "preact/hooks";
+import { ComponentChildren} from "preact";
 import { logout } from "./common";
 import Cart from "./cart";
 import { Product, ProductData } from "./payment_common";
@@ -15,11 +16,28 @@ const NavItem = ({ url, icon, children }: { url: string, icon: string, children:
 }
 
 export const Sidebar = ({ cart, className = "" }: { cart: { cart: Cart, productData: ProductData } | null, className?: string }) => {
+    const sidebarRef = useRef<HTMLDivElement>(null);
+    const [isSidebarScrollable, setIsSidebarScrollable] = useState(false);
+
+    useEffect((): (() => void) | void => {
+        const checkSidebarHeight = () => {
+            if(sidebarRef.current) {
+                setIsSidebarScrollable(sidebarRef.current.scrollHeight > sidebarRef.current.clientHeight);
+            }
+        }
+        checkSidebarHeight();
+        window.addEventListener("resize", checkSidebarHeight);
+
+        return () => {
+            window.removeEventListener("resize", checkSidebarHeight);
+        };
+    }, []);
+
     let path = location.pathname.trim();
     if (path.endsWith("/")) {
         path = path.substring(0, path.length - 1);
     }
-    return <div id="left-sidebar">
+    return <div id="left-sidebar" ref={sidebarRef} className={isSidebarScrollable ? "scrollable" : ""}>
         <div className={`sidebar-fixed-content ${className}`}>
             <img className="makerspace-logo" src={`${window.staticBasePath}/images/logo-transparent-500px-300x210.png`} />
             <ul className="uk-nav uk-nav-default">
@@ -36,6 +54,7 @@ export const Sidebar = ({ cart, className = "" }: { cart: { cart: Cart, productD
                 <NavItem url="/shop/member/history" icon="history">Min köphistorik</NavItem>
                 <NavItem url="/shop/member/courses" icon="star">Kurser</NavItem>
                 <NavItem url="/shop/member/licenses" icon="tag">Licenser och rabatter</NavItem>
+                {/*<NavItem url="#" icon="cart">Presentkort</NavItem>*/}
                 <li>
                     <a onClick={e => {
                         e.preventDefault();
@@ -43,9 +62,12 @@ export const Sidebar = ({ cart, className = "" }: { cart: { cart: Cart, productD
                     }}><span uk-icon="sign-out"></span> Logga ut</a>
                 </li>
 
-                {cart !== null && <li className={`uk-button uk-button-primary sidebar-buy-btn ${cart.cart.items.length === 0 ? 'cart-empty' : ''}`}>
-                    <a href="/shop/cart"><span uk-icon="cart"></span> Betala<span id="cart-sum">{Cart.formatCurrency(cart.cart.sum(cart.productData.id2item))}</span></a>
-                </li>
+                {cart !== null &&
+                    <li className={`uk-button uk-button-primary sidebar-buy-btn ${cart.cart.items.length === 0 ? 'cart-empty' : ''}`}>
+                        <a href="/shop/cart"><span uk-icon="cart"></span> Betala
+                            <span id="cart-sum">{Cart.formatCurrency(cart.cart.sum(cart.productData.id2item))}</span>
+                        </a>
+                    </li>
                 }
             </ul>
         </div>
