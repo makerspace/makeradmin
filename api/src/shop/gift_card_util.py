e33202b69d15c7f16884adc956e0efa679f0c4c4
Arman <arman.iqbal@prevas.se>
2023-12-11 06:49:05 +0100

    [FR-206] Remove null check on gift_card.email. Better subject for email.

--- api/src/shop/gift_card_util.py
+++ api/src/shop/gift_card_util.py
@@ -23,14 +23,11 @@ def send_gift_card_email(gift_card):
     Parameters:
     - gift_card: The gift card to send an email for.
     """
-    if gift_card.email:
-        send_message(
-            template=MessageTemplate.GIFT_CARD_PURCHASE,
-            member=None,
-            recipient_email=gift_card.email,
-            gift_card=gift_card,
-        )
-    else:
-        # TODO: Log this error
-        # Do I need to check for this case? Is it even possible?
-        print("Error: Gift card does not have a valid recipient email.")
+
+    send_message(
+        template=MessageTemplate.GIFT_CARD_PURCHASE,
+        member=None,
+        recipient_email=gift_card.email,
+        gift_card=gift_card,
+    )
+
