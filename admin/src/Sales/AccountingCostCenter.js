1ebeac10ed65d7c1eeba21bc8b3743d238e8a93b
ESiggstedt <ellen.siggstedt@prevas.se>
2024-01-12 13:26:12 +0100

    Formatting with recommended format tools

--- admin/src/Sales/AccountingCostCenter.js
+++ admin/src/Sales/AccountingCostCenter.js
@@ -1,25 +1,30 @@
-import React from 'react';
+import React from "react";
 import Collection from "../Models/Collection";
 import CollectionTable from "../Components/CollectionTable";
 import CollectionNavigation from "../Models/CollectionNavigation";
 import TextInput from "../Components/TextInput";
-import TransactionCostCenter from '../Models/TransactionCostCenter';
+import TransactionCostCenter from "../Models/TransactionCostCenter";
 import SearchBox from "../Components/SearchBox";
-import Textarea from '../Components/Textarea';
+import Textarea from "../Components/Textarea";
 
 class AccountingCostCenter extends CollectionNavigation {
-
     constructor(props) {
         super(props);
         this.state = { saveEnabled: false };
         this.transactionCostCenter = new TransactionCostCenter();
         const { search, page } = this.state;
-        this.collection = new Collection({ type: TransactionCostCenter, search: search, page: page });
+        this.collection = new Collection({
+            type: TransactionCostCenter,
+            search: search,
+            page: page,
+        });
     }
 
     componentDidMount() {
         const transactionCostCenter = this.transactionCostCenter;
-        this.unsubscribe = transactionCostCenter.subscribe(() => this.setState({ saveEnabled: transactionCostCenter.canSave() }));
+        this.unsubscribe = transactionCostCenter.subscribe(() =>
+            this.setState({ saveEnabled: transactionCostCenter.canSave() }),
+        );
     }
 
     componentWillUnmount() {
@@ -27,12 +32,10 @@ class AccountingCostCenter extends CollectionNavigation {
     }
 
     createCostCenter() {
-        this.transactionCostCenter
-            .save()
-            .then(() => {
-                this.transactionCostCenter.reset();
-                this.collection.fetch();
-            });
+        this.transactionCostCenter.save().then(() => {
+            this.transactionCostCenter.reset();
+            this.collection.fetch();
+        });
     }
 
     render() {
@@ -42,17 +45,45 @@ class AccountingCostCenter extends CollectionNavigation {
             <div>
                 <div className="uk-margin-top">
                     <h2>Hantera kostnadsställe</h2>
-                    <p>På denna sida kan du lägga till och ta bort kostnadsställen.</p>
+                    <p>
+                        På denna sida kan du lägga till och ta bort
+                        kostnadsställen.
+                    </p>
                 </div>
 
                 <div className="meep">
-                    <form className="uk-form uk-margin-bottom" onSubmit={(e) => { e.preventDefault(); this.createCostCenter(); return false; }}>
-                        <TextInput model={this.transactionCostCenter} name="cost_center" title="Namn" placeholder="Namn på nytt kostnadsställe" />
-                        <Textarea model={this.transactionCostCenter} name="description" title="Beskrivning" placeholder="Beskrivning av nytt kostnadsställe" />
+                    <form
+                        className="uk-form uk-margin-bottom"
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            this.createCostCenter();
+                            return false;
+                        }}
+                    >
+                        <TextInput
+                            model={this.transactionCostCenter}
+                            name="cost_center"
+                            title="Namn"
+                            placeholder="Namn på nytt kostnadsställe"
+                        />
+                        <Textarea
+                            model={this.transactionCostCenter}
+                            name="description"
+                            title="Beskrivning"
+                            placeholder="Beskrivning av nytt kostnadsställe"
+                        />
 
                         <div className="uk-form-row uk-margin-top">
                             <div className="uk-form-controls">
-                                <button className="uk-button uk-button-success uk-float-right" disabled={!saveEnabled}><i className="uk-icon-save" /> {this.transactionCostCenter.id ? 'Spara nytt kostnadsställe' : 'Skapa nytt kostnadsställe'}</button>
+                                <button
+                                    className="uk-button uk-button-success uk-float-right"
+                                    disabled={!saveEnabled}
+                                >
+                                    <i className="uk-icon-save" />{" "}
+                                    {this.transactionCostCenter.id
+                                        ? "Spara nytt kostnadsställe"
+                                        : "Skapa nytt kostnadsställe"}
+                                </button>
                             </div>
                         </div>
                     </form>
@@ -69,13 +100,20 @@ class AccountingCostCenter extends CollectionNavigation {
                             { title: "Beskrivning", sort: "description" },
                             { title: "" },
                         ]}
-                        rowComponent={({ item, deleteItem }) =>
+                        rowComponent={({ item, deleteItem }) => (
                             <tr>
                                 <td>{item.cost_center}</td>
                                 <td>{item.description}</td>
-                                <td><a onClick={() => deleteItem(item)} className="removebutton"><i className="uk-icon-trash" /></a></td>
+                                <td>
+                                    <a
+                                        onClick={() => deleteItem(item)}
+                                        className="removebutton"
+                                    >
+                                        <i className="uk-icon-trash" />
+                                    </a>
+                                </td>
                             </tr>
-                        }
+                        )}
                         onPageNav={this.onPageNav}
                     />
                 </div>
