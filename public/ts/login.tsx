bee39a5b3e2fdff5f15657b1d52fecf2c50caf4b
Aron Granberg <aron.granberg@gmail.com>
2024-02-18 21:01:14 +0100

    Fix quiz page used its own login component.

--- public/ts/login.tsx
+++ public/ts/login.tsx
@@ -99,7 +99,7 @@ export function render_login(
     heading: string | null,
     redirect: string | null,
 ) {
-    render(<Login heading={heading} redirect={redirect} />, root);
+    render(<LoginPage heading={heading} redirect={redirect} />, root);
 }
 
 enum LoginMethod {
@@ -107,14 +107,7 @@ enum LoginMethod {
     EmailAndPassword = "EmailAndPassword",
 }
 
-function Login({
-    heading,
-    redirect,
-}: {
-    heading: string | null;
-    redirect: string | null;
-}) {
-    heading = heading || "Logga in";
+export const Login = ({ redirect }: { redirect: string | null }) => {
     const [tag, setTag] = useState("");
     const [password, setPassword] = useState("");
 
@@ -127,90 +120,106 @@ function Login({
 
     return (
         <>
+            <form
+                class="uk-form"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    // Error handling
+                    if (!tag) {
+                        UIkit.modal.alert(
+                            "Du måste fylla i din E-postadress",
+                        );
+                        return;
+                    }
+
+                    if (loginMethod == LoginMethod.EmailAndPassword && !password) {
+                        UIkit.modal.alert(
+                            "Du måste fylla i ditt lösenord",
+                        );
+                        return;
+                    }
+
+                    localStorage.setItem("last_login_method", loginMethod);
+
+                    if (loginMethod === LoginMethod.EmailLink) {
+                        login_via_single_use_link(tag.trim(), redirect);
+                    } else {
+                        login_via_password(tag.trim(), password, redirect);
+                    }
+                }}
+            >
+                <div class="uk-form-row" style="margin: 16px 0;">
+                    <input
+                        autoFocus
+                        class="uk-form-large uk-width-1-1"
+                        type="text"
+                        id="email"
+                        placeholder="Email/Medlemsnummer"
+                        value={tag}
+                        onChange={(e) =>
+                            setTag(e.currentTarget.value)
+                        }
+                    />
+                </div>
+
+                <div class={"uk-form-row password-field " + (loginMethod === LoginMethod.EmailLink ? "hidden" : "")}>
+                    <input
+                        autoFocus
+                        class="uk-form-large uk-width-1-1"
+                        type="password"
+                        id="password"
+                        placeholder="Lösenord"
+                        value={password}
+                        onChange={(e) =>
+                            setPassword(e.currentTarget.value)
+                        }
+                    />
+                </div>
+
+                <div class="uk-form-row" style="margin: 16px 0;">
+                    <button
+                        type="submit"
+                        class="uk-width-1-1 uk-button uk-button-primary uk-button-large"
+                    >
+                        <span class="uk-icon-check" />
+                        Gå vidare
+                    </button>
+                </div>
+            </form>
+            <p style="text-align: center;"><a role="button" onClick={() => {
+                if (loginMethod === LoginMethod.EmailAndPassword) {
+                    // Clear password when switching away from password login
+                    setPassword("");
+                }
+                setLoginMethod(loginMethod === LoginMethod.EmailLink ? LoginMethod.EmailAndPassword : LoginMethod.EmailLink);
+            }}>{loginMethod === LoginMethod.EmailLink ? "Logga in med lösenord" : "Logga in med endast email"}</a>
+            </p>
+            <p style="text-align: center;">
+                <a href="/shop/register">
+                    Bli medlem / Become a member
+                </a>
+            </p>
+        </>
+    )
+}
+
+function LoginPage({
+    heading,
+    redirect,
+}: {
+    heading: string | null;
+    redirect: string | null;
+}) {
+    heading = heading || "Logga in";
+
+    return (
+        <>
             <Sidebar cart={null} />
             <div id="content">
                 <div class="content-centering">
                     <div class="uk-width-medium">
                         <h1 style="text-align: center;">{heading}</h1>
-                        <form
-                            class="uk-form"
-                            onSubmit={(e) => {
-                                e.preventDefault();
-                                // Error handling
-                                if (!tag) {
-                                    UIkit.modal.alert(
-                                        "Du måste fylla i din E-postadress",
-                                    );
-                                    return;
-                                }
-
-                                if (loginMethod == LoginMethod.EmailAndPassword && !password) {
-                                    UIkit.modal.alert(
-                                        "Du måste fylla i ditt lösenord",
-                                    );
-                                    return;
-                                }
-
-                                localStorage.setItem("last_login_method", loginMethod);
-
-                                if (loginMethod === LoginMethod.EmailLink) {
-                                    login_via_single_use_link(tag.trim(), redirect);
-                                } else {
-                                    login_via_password(tag.trim(), password, redirect);
-                                }
-                            }}
-                        >
-                            <div class="uk-form-row" style="margin: 16px 0;">
-                                <input
-                                    autoFocus
-                                    class="uk-form-large uk-width-1-1"
-                                    type="text"
-                                    id="email"
-                                    placeholder="Email/Medlemsnummer"
-                                    value={tag}
-                                    onChange={(e) =>
-                                        setTag(e.currentTarget.value)
-                                    }
-                                />
-                            </div>
-
-                            <div class={"uk-form-row password-field " + (loginMethod === LoginMethod.EmailLink ? "hidden" : "")} style="margin: 16px 0;">
-                                <input
-                                    autoFocus
-                                    class="uk-form-large uk-width-1-1"
-                                    type="password"
-                                    id="password"
-                                    placeholder="Lösenord"
-                                    value={password}
-                                    onChange={(e) =>
-                                        setPassword(e.currentTarget.value)
-                                    }
-                                />
-                            </div>
-
-                            <div class="uk-form-row" style="margin: 16px 0;">
-                                <button
-                                    type="submit"
-                                    class="uk-width-1-1 uk-button uk-button-primary uk-button-large"
-                                >
-                                    <span class="uk-icon-check" />
-                                    Gå vidare
-                                </button>
-                            </div>
-                        </form>
-                        <p style="text-align: center;"><a role="button" onClick={() => {
-                            if (loginMethod === LoginMethod.EmailAndPassword) {
-                                // Clear password when switching away from password login
-                                setPassword("");
-                            }
-                            setLoginMethod(loginMethod === LoginMethod.EmailLink ? LoginMethod.EmailAndPassword : LoginMethod.EmailLink);
-                        }}>{loginMethod === LoginMethod.EmailLink ? "Logga in med lösenord" : "Logga in med endast email"}</a>
-                        </p>
-                        <p style="text-align: center;">
-                            <a href="/shop/register">
-                                Bli medlem / Become a member
-                            </a>
-                        </p>
+                        <Login redirect={redirect} />
                     </div>
                 </div>
             </div>
