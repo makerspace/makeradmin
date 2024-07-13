597da3906fd4118b607b2422f29d2476459453b8
Aleksandar Ratesic <aratesic@gmail.com>
2024-07-13 18:32:03 +0200

    Add tests

--- api/src/systest/api/purchase_test.py
+++ api/src/systest/api/purchase_test.py
@@ -1,6 +1,7 @@
 from logging import getLogger
 
 import stripe
+from membership.models import Member, Span
 from shop.transactions import CartItem, Purchase
 from test_aid.systest_base import VALID_3DS_CARD_NO, VALID_NON_3DS_CARD_NO, ApiShopTestMixin, ApiTest, retry
 
@@ -135,7 +136,48 @@ class Test(ApiShopTestMixin, ApiTest):
         self.post(f"/webshop/pay", purchase.to_dict(), token=self.token).expect(code=400, what="empty_cart")
 
     def test_labaccess_purchase_fails_without_base_membership(self):
-        pass
+        category = self.db.create_category()
+        lab_access_product = self.db.create_product(
+            price=575,
+            unit="mån",
+            category_id=category.id,
+            product_metadata={
+                "allowed_price_levels": ["low_income_discount"],
+                "special_product_id": "single_labaccess_month",
+            },
+        )
+        purchase = Purchase(
+            cart=[CartItem(lab_access_product.id, 1)],
+            expected_sum=lab_access_product.price,
+            stripe_payment_method_id="not_used",
+        )
+        member = self.db.create_member()
+        self.post("/webshop/pay", purchase.to_dict(), token=self.token).expect(code=400)
 
     def test_labaccess_purchase_allowed_together_with_base_membership(self):
-        pass
+        category = self.db.create_category()
+        base_membership = self.db.create_product(
+            price=200,
+            unit="år",
+            category_id=category.id,
+            product_metadata={
+                "allowed_price_levels": ["low_income_discount"],
+                "special_product_id": "single_membership_year",
+            },
+        )
+        lab_access_product = self.db.create_product(
+            price=575,
+            unit="mån",
+            category_id=category.id,
+            product_metadata={
+                "allowed_price_levels": ["low_income_discount"],
+                "special_product_id": "single_labaccess_month",
+            },
+        )
+        purchase = Purchase(
+            cart=[CartItem(lab_access_product.id, 1), CartItem(base_membership.id, 1)],
+            expected_sum=lab_access_product.price + base_membership.price,
+            stripe_payment_method_id="not_used",
+        )
+        member = self.db.create_member()
+        self.post("/webshop/pay", purchase.to_dict(), token=self.token).expect(code=200)
