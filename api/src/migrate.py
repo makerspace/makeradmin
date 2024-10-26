94a894c0385aa7eed411b0301ab94f24414a718a
Anders Roos <anders.roos@gmail.com>
2024-10-26 15:51:19 +0200

    fix

--- api/src/migrate.py
+++ api/src/migrate.py
@@ -7,7 +7,7 @@ from os import listdir
 from os.path import dirname, exists, isdir, join
 
 from service.logging import logger
-from sqlalchemy import inspect
+from sqlalchemy import inspect, text
 
 Migration = namedtuple("Migration", "id,name")
 
@@ -26,53 +26,71 @@ def ensure_migrations_table(engine, session_factory):
     if "migrations" not in table_names:
         with closing(session_factory()) as session:
             logger.info("creating migrations table")
-            session.execute("ALTER DATABASE makeradmin CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci")
+            session.execute(text("ALTER DATABASE makeradmin CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci"))
             session.execute(
-                "CREATE TABLE migrations ("
-                "    id INTEGER NOT NULL,"
-                "    name VARCHAR(255) COLLATE utf8mb4_0900_ai_ci NOT NULL,"
-                "    applied_at DATETIME NOT NULL,"
-                "    PRIMARY KEY (id)"
-                ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci"
+                text(
+                    "CREATE TABLE migrations ("
+                    "    id INTEGER NOT NULL,"
+                    "    name VARCHAR(255) COLLATE utf8mb4_0900_ai_ci NOT NULL,"
+                    "    applied_at DATETIME NOT NULL,"
+                    "    PRIMARY KEY (id)"
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci"
+                )
             )
             session.commit()
     elif "service" in {c["name"] for c in engine_inspect.get_columns("migrations")}:
         with closing(session_factory()) as session:
             logger.info("updating existing migrations table")
             session.execute(
-                "UPDATE migrations SET id=1, name='0001_initial_core'"
-                "     WHERE id=1 AND service='core' AND name='0001_initial'"
+                text(
+                    "UPDATE migrations SET id=1, name='0001_initial_core'"
+                    "     WHERE id=1 AND service='core' AND name='0001_initial'"
+                )
             )
             session.execute(
-                "UPDATE migrations SET id=5, name='0005_remove_excessive_permissions'"
-                "    WHERE id=2 AND service='membership' AND name='0002_remove_excessive_permissions'"
+                text(
+                    "UPDATE migrations SET id=5, name='0005_remove_excessive_permissions'"
+                    "    WHERE id=2 AND service='membership' AND name='0002_remove_excessive_permissions'"
+                )
             )
             session.execute(
-                "UPDATE migrations SET id=6, name='0006_add_box'"
-                "    WHERE id=3 AND service='membership' AND name='0003_add_box'"
+                text(
+                    "UPDATE migrations SET id=6, name='0006_add_box'"
+                    "    WHERE id=3 AND service='membership' AND name='0003_add_box'"
+                )
             )
             session.execute(
-                "UPDATE migrations SET id=2, name='0002_initial_membership'"
-                "    WHERE id=1 AND service='membership' AND name='0001_initial'"
+                text(
+                    "UPDATE migrations SET id=2, name='0002_initial_membership'"
+                    "    WHERE id=1 AND service='membership' AND name='0001_initial'"
+                )
             )
             session.execute(
-                "UPDATE migrations SET id=4, name='0004_initial_messages'"
-                "    WHERE id=1 AND service='messages' AND name='0001_initial'"
+                text(
+                    "UPDATE migrations SET id=4, name='0004_initial_messages'"
+                    "    WHERE id=1 AND service='messages' AND name='0001_initial'"
+                )
             )
             session.execute(
-                "UPDATE migrations SET id=7, name='0007_rename_everything'"
-                "    WHERE id=2 AND service='messages' AND name='0002_rename_everything'"
+                text(
+                    "UPDATE migrations SET id=7, name='0007_rename_everything'"
+                    "    WHERE id=2 AND service='messages' AND name='0002_rename_everything'"
+                )
             )
             session.execute(
-                "UPDATE migrations SET id=3, name='0003_initial_shop'"
-                "    WHERE id=1 AND service='shop' AND name='0001_initial'"
+                text(
+                    "UPDATE migrations SET id=3, name='0003_initial_shop'"
+                    "    WHERE id=1 AND service='shop' AND name='0001_initial'"
+                )
             )
             session.execute(
-                "UPDATE migrations SET id=8, name='0008_password_reset_token'"
-                "    WHERE id=2 AND service='core' AND name='0002_password_reset_token'"
+                text(
+                    "UPDATE migrations SET id=8, name='0008_password_reset_token'"
+                    "    WHERE id=2 AND service='core' AND name='0002_password_reset_token'"
+                )
             )
-            session.execute("ALTER TABLE migrations DROP PRIMARY KEY, ADD PRIMARY KEY(id)")
-            session.execute("ALTER TABLE migrations DROP COLUMN service")
+            session.execute(text("ALTER TABLE migrations DROP PRIMARY KEY, ADD PRIMARY KEY(id)"))
+            session.execute(text("ALTER TABLE migrations DROP COLUMN service"))
             session.commit()
 
 
@@ -99,7 +117,7 @@ def run_migrations(session_factory):
 
         migrations.sort(key=lambda m: m.id)
 
-        applied = {i: Migration(i, n) for i, n in session.execute("SELECT id, name FROM migrations ORDER BY ID")}
+        applied = {i: Migration(i, n) for i, n in session.execute(text("SELECT id, name FROM migrations ORDER BY ID"))}
         session.commit()
 
         logger.info(f"{len(migrations) - len(applied)} migrations to apply, {len(applied)} migrations already applied")
@@ -115,10 +133,10 @@ def run_migrations(session_factory):
                 logger.info(f"migrations, applying {migration.name}")
 
                 for sql in read_sql(join(migrations_dir, migration.name + ".sql")):
-                    session.execute(sql)
+                    session.execute(text(sql))
 
                 session.execute(
-                    "INSERT INTO migrations VALUES (:id, :name, :applied_at)",
+                    text("INSERT INTO migrations VALUES (:id, :name, :applied_at)"),
                     {
                         "id": migration.id,
                         "name": migration.name,
