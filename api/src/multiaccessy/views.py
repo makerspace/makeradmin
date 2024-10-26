2e28f18be02b68bb2ff553ef4fbd7c461186cd4a
Aron Granberg <aron.granberg@gmail.com>
2024-10-26 15:21:35 +0200

    Remove duplicate code.

--- api/src/multiaccessy/views.py
+++ api/src/multiaccessy/views.py
@@ -219,5 +219,3 @@ def accessy_webhook() -> None:
         return None
     else:
         raise UnprocessableEntity("Invalid signature")
-        raise UnprocessableEntity("Invalid signature")
-        raise UnprocessableEntity("Invalid signature")
