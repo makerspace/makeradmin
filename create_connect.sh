3e818c229c5ff00f0d1708d4beb9e2061e892418
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2024-11-17 16:47:55 +0100

    Fix shellcheck SC2006 and SC2046

--- create_connect.sh
+++ create_connect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker compose -f docker-compose.yml -f docker-compose.prod.yml up --no-start api admin public && \
-docker network connect makeradmin-bridge `docker compose ps --all -q api` && \
-docker network connect makeradmin-bridge `docker compose ps --all -q admin` && \
-docker network connect makeradmin-bridge `docker compose ps --all -q public`
+docker network connect makeradmin-bridge "$(docker compose ps --all -q api)" && \
+docker network connect makeradmin-bridge "$(docker compose ps --all -q admin)" && \
+docker network connect makeradmin-bridge "$(docker compose ps --all -q public)"
