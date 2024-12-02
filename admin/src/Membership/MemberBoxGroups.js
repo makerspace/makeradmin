73358461ed3ede96c0be7d80a5fffe1cbd8c4fc9
Guanjie <user.name>
2024-12-02 13:48:39 -0500

    refactor MemberBoxGroups

--- admin/src/Membership/MemberBoxGroups.js
+++ admin/src/Membership/MemberBoxGroups.js
@@ -1,133 +1,108 @@
-import React from "react";
-import Collection from "../Models/Collection";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import Group from "../Models/Group";
-import CollectionTable from "../Components/CollectionTable";
-import { get } from "../gateway";
 import Select from "react-select";
 import * as _ from "underscore";
+import CollectionTable from "../Components/CollectionTable";
+import Collection from "../Models/Collection";
+import Group from "../Models/Group";
 
 const filterOptions = (items, options) => {
     const current = new Set(items.map((i) => i.id));
     return options.filter((o) => !current.has(o.group_id));
 };
 
-const updateItems = (items) => (prevState) => {
-    return {
-        showOptions: filterOptions(items, prevState.options),
-        items,
-    };
-};
+function MemberBoxGroups(props) {
+    const collection = new Collection({
+        type: Group,
+        url: `/membership/member/${props.match.params.member_id}/groups`,
+        idListName: "groups",
+        pageSize: 0,
+    });
 
-const updateOptions = (options) => (prevState) => {
-    return {
-        showOptions: filterOptions(prevState.items, options),
-        options,
-    };
-};
+    const [items, setItems] = useState([]);
+    const [options, setOptions] = useState([]);
+    const [showOptions, setShowOptions] = useState([]);
+    const [selectedOption, setSelectedOption] = useState(null);
 
-class MemberBoxGroups extends React.Component {
-    constructor(props) {
-        super(props);
-        this.collection = new Collection({
-            type: Group,
-            url: `/membership/member/${props.match.params.member_id}/groups`,
-            idListName: "groups",
-            pageSize: 0,
+    useEffect(() => {
+        get({ url: "/membership/group" }).then((data) => {
+            const updatedOptions = data.data;
+            setOptions(updatedOptions);
+            setShowOptions(filterOptions(items, updatedOptions));
         });
-        this.state = {
-            items: [],
-            options: [],
-            showOptions: [],
-            selectedOption: null,
+
+        const unsubscribe = collection.subscribe(({ items: newItems }) => {
+            setItems(newItems || []);
+            setShowOptions(filterOptions(newItems || [], options));
+        });
+
+        return () => {
+            unsubscribe();
         };
+    }, []);
 
-        get({ url: "/membership/group" }).then((data) =>
-            this.setState(updateOptions(data.data)),
-        );
-    }
-
-    componentDidMount() {
-        this.unsubscribe = this.collection.subscribe(({ items }) =>
-            this.setState(updateItems(items || [])),
-        );
-    }
-
-    componentWillUnmount() {
-        this.unsubscribe();
-    }
-
-    selectOption(member_id, group) {
-        this.setState({ selectedOption: group });
+    const selectOption = (group) => {
+        setSelectedOption(group);
 
         if (_.isEmpty(group)) {
             return;
         }
 
-        this.collection
-            .add(new Group(group))
-            .then(this.setState({ selectedOption: null }));
-    }
+        collection.add(new Group(group)).then(() => {
+            setSelectedOption(null);
+        });
+    };
 
-    render() {
-        const { member_id } = this.props.match.params;
-        const { selectedOption, showOptions } = this.state;
-
-        return (
-            <div>
-                <div className="uk-margin-top uk-form uk-form-stacked">
-                    <label className="uk-form-label" htmlFor="group">
-                        Lägg till i grupp
-                    </label>
-                    <div className="uk-form-controls">
-                        <Select
-                            name="group"
-                            className="uk-select"
-                            tabIndex={1}
-                            options={showOptions}
-                            value={selectedOption}
-                            getOptionValue={(g) => g.group_id}
-                            getOptionLabel={(g) => g.title}
-                            onChange={(group) =>
-                                this.selectOption(member_id, group)
-                            }
-                        />
-                    </div>
-                </div>
-                <div className="uk-margin-top">
-                    <CollectionTable
-                        emptyMessage="Inte med i några grupper"
-                        collection={this.collection}
-                        columns={[
-                            { title: "Titel", sort: "title" },
-                            { title: "Antal medlemmar" },
-                            { title: "" },
-                        ]}
-                        rowComponent={({ item }) => (
-                            <tr>
-                                <td>
-                                    <Link to={"/membership/groups/" + item.id}>
-                                        {item.title}
-                                    </Link>
-                                </td>
-                                <td>{item.num_members}</td>
-                                <td>
-                                    <a
-                                        onClick={() =>
-                                            this.collection.remove(item)
-                                        }
-                                        className="removebutton"
-                                    >
-                                        <i className="uk-icon-trash" />
-                                    </a>
-                                </td>
-                            </tr>
-                        )}
+    return (
+        <div>
+            <div className="uk-margin-top uk-form uk-form-stacked">
+                <label className="uk-form-label" htmlFor="group">
+                    Lägg till i grupp
+                </label>
+                <div className="uk-form-controls">
+                    <Select
+                        name="group"
+                        className="uk-select"
+                        tabIndex={1}
+                        options={showOptions}
+                        value={selectedOption}
+                        getOptionValue={(g) => g.group_id}
+                        getOptionLabel={(g) => g.title}
+                        onChange={(g) => selectOption(g)}
                     />
                 </div>
             </div>
-        );
-    }
+            <div className="uk-margin-top">
+                <CollectionTable
+                    emptyMessage="Inte med i några grupper"
+                    collection={collection}
+                    columns={[
+                        { title: "Titel", sort: "title" },
+                        { title: "Antal medlemmar" },
+                        { title: "" },
+                    ]}
+                    rowComponent={({ item }) => (
+                        <tr>
+                            <td>
+                                <Link to={`/membership/groups/${item.id}`}>
+                                    {item.title}
+                                </Link>
+                            </td>
+                            <td>{item.num_members}</td>
+                            <td>
+                                <a
+                                    onClick={() => collection.remove(item)}
+                                    className="removebutton"
+                                >
+                                    <i className="uk-icon-trash" />
+                                </a>
+                            </td>
+                        </tr>
+                    )}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default MemberBoxGroups;
