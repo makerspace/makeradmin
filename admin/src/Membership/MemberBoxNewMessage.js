3ac3fc633717ca63f78c2406eb00eb96e77fc235
Guanjie <user.name>
2024-12-02 19:27:55 -0500

    refactor Member Box New Message

--- admin/src/Membership/MemberBoxNewMessage.js
+++ admin/src/Membership/MemberBoxNewMessage.js
@@ -1,39 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { withRouter } from "react-router";
+import { useParams } from "react-router-dom";
 import MessageForm from "../Components/MessageForm";
 import Message from "../Models/Message";
-import { notifySuccess } from "../message";
-import { withRouter } from "react-router";
 import { browserHistory } from "../browser_history";
+import { notifySuccess } from "../message";
 
-class MemberBoxMessages extends React.Component {
-    constructor(props) {
-        super(props);
-        this.message = new Message({
-            recipients: [{ type: "member", id: props.match.params.member_id }],
-        });
-    }
+const MemberBoxMessages = () => {
+    const { member_id } = useParams();
+    const [message] = useState(
+        new Message({
+            recipients: [{ type: "member", id: member_id }],
+        }),
+    );
 
-    onSend() {
-        const params = this.props.match.params;
-        this.message.save().then(() => {
-            browserHistory.push(
-                "/membership/members/" + params.member_id + "/messages",
-            );
+    const onSend = () => {
+        message.save().then(() => {
+            browserHistory.push(`/membership/members/${member_id}/messages`);
             notifySuccess("Ditt meddelande har skickats");
         });
-    }
+    };
 
-    render() {
-        return (
-            <div className="uk-margin-top">
-                <MessageForm
-                    recipientSelect={false}
-                    message={this.message}
-                    onSave={() => this.onSend()}
-                />
-            </div>
-        );
-    }
-}
+    return (
+        <div className="uk-margin-top">
+            <MessageForm
+                recipientSelect={false}
+                message={message}
+                onSave={onSend}
+            />
+        </div>
+    );
+};
 
 export default withRouter(MemberBoxMessages);
