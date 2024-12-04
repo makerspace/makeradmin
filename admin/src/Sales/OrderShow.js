1af1548f8bf10afca163390b2b55543eebab4fa7
sofiareyesfranco <165427980+sofiareyesfranco@users.noreply.github.com>
2024-12-04 02:27:38 -0500

    changed class based to function based  admin/src/Sales/OrderShow.js:11

--- admin/src/Sales/OrderShow.js
+++ admin/src/Sales/OrderShow.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 import Order from "../Models/Order";
 import Collection from "../Models/Collection";
 import CollectionTable from "../Components/CollectionTable";
@@ -8,117 +8,102 @@ import OrderAction from "../Models/OrderAction";
 import Currency from "../Components/Currency";
 import { dateTimeToStr } from "../utils";
 
-class OrderShow extends React.Component {
-    constructor(props) {
-        super(props);
-        const { id } = props.match.params;
-        this.order = Order.get(id);
-        this.state = {};
-        this.orderRows = new Collection({
-            type: OrderRow,
-            url: `/webshop/transaction/${id}/contents`,
-            pageSize: 0,
-            expand: "product",
+const OrderShow = () => {
+    const { id } = useParams(); // Get id from URL params
+    const [memberId, setMemberId] = useState(null);
+
+    const order = Order.get(id);
+    const orderRows = new Collection({
+        type: OrderRow,
+        url: `/webshop/transaction/${id}/contents`,
+        pageSize: 0,
+        expand: "product",
+    });
+    const orderActions = new Collection({
+        type: OrderAction,
+        url: `/webshop/transaction/${id}/actions`,
+        pageSize: 0,
+    });
+
+    useEffect(() => {
+        const unsubscribe = order.subscribe(() => {
+            const { member_id } = order;
+            setMemberId(member_id);
         });
-        this.orderActions = new Collection({
-            type: OrderAction,
-            url: `/webshop/transaction/${id}/actions`,
-            pageSize: 0,
-        });
-    }
 
-    componentDidMount() {
-        this.unsubscribe = this.order.subscribe(() => {
-            const { member_id } = this.order;
-            this.setState({ member_id });
-        });
-    }
+        return () => unsubscribe(); // Cleanup on component unmount
+    }, [order]);
 
-    componentWillUnmount() {
-        this.unsubscribe();
-    }
-
-    render() {
-        const { member_id } = this.state;
-        const { id } = this.props.match.params;
-
-        return (
-            <div>
-                <div className="uk-margin-top">
-                    <h2>Order #{id}</h2>
-                    <div>
-                        <h3>Medlem</h3>
-                        <Link to={"/membership/members/" + member_id}>
-                            member_id {member_id}
-                        </Link>
-                    </div>
-                </div>
-                <div className="uk-margin-top">
-                    <h3>Orderrader</h3>
-                    <CollectionTable
-                        emptyMessage="Listan är tom"
-                        collection={this.orderRows}
-                        columns={[
-                            { title: "Produkt" },
-                            { title: "Pris", class: "uk-text-right" },
-                            { title: "Antal" },
-                            { title: "Summa", class: "uk-text-right" },
-                        ]}
-                        rowComponent={({ item }) => (
-                            <tr>
-                                <td>
-                                    <Link
-                                        to={"/sales/product/" + item.product_id}
-                                    >
-                                        {item.name}
-                                    </Link>
-                                </td>
-                                <td className="uk-text-right">
-                                    <Currency
-                                        value={(100 * item.amount) / item.count}
-                                    />{" "}
-                                    kr
-                                </td>
-                                <td>{item.count}</td>
-                                <td className="uk-text-right">
-                                    <Currency value={100 * item.amount} /> kr
-                                </td>
-                            </tr>
-                        )}
-                    />
-                </div>
-                <div className="uk-margin-top">
-                    <h3>Ordereffekter</h3>
-                    <CollectionTable
-                        emptyMessage="Listan är tom"
-                        collection={this.orderActions}
-                        columns={[
-                            { title: "Orderrad" },
-                            { title: "Åtgärd" },
-                            { title: "Antal", class: "uk-text-right" },
-                            { title: "Utförd", class: "uk-text-right" },
-                        ]}
-                        rowComponent={({ item }) => {
-                            return (
-                                <tr>
-                                    <td>{item.id}</td>
-                                    <td>{item.action_type}</td>
-                                    <td className="uk-text-right">
-                                        {item.value}
-                                    </td>
-                                    <td className="uk-text-right">
-                                        {item.completed_at
-                                            ? dateTimeToStr(item.completed_at)
-                                            : "pending"}
-                                    </td>
-                                </tr>
-                            );
-                        }}
-                    />
+    return (
+        <div>
+            <div className="uk-margin-top">
+                <h2>Order #{id}</h2>
+                <div>
+                    <h3>Medlem</h3>
+                    <Link to={`/membership/members/${memberId}`}>
+                        member_id {memberId}
+                    </Link>
                 </div>
             </div>
-        );
-    }
-}
+            <div className="uk-margin-top">
+                <h3>Orderrader</h3>
+                <CollectionTable
+                    emptyMessage="Listan är tom"
+                    collection={orderRows}
+                    columns={[
+                        { title: "Produkt" },
+                        { title: "Pris", class: "uk-text-right" },
+                        { title: "Antal" },
+                        { title: "Summa", class: "uk-text-right" },
+                    ]}
+                    rowComponent={({ item }) => (
+                        <tr key={item.product_id}>
+                            <td>
+                                <Link to={`/sales/product/${item.product_id}`}>
+                                    {item.name}
+                                </Link>
+                            </td>
+                            <td className="uk-text-right">
+                                <Currency
+                                    value={(100 * item.amount) / item.count}
+                                />{" "}
+                                kr
+                            </td>
+                            <td>{item.count}</td>
+                            <td className="uk-text-right">
+                                <Currency value={100 * item.amount} /> kr
+                            </td>
+                        </tr>
+                    )}
+                />
+            </div>
+            <div className="uk-margin-top">
+                <h3>Ordereffekter</h3>
+                <CollectionTable
+                    emptyMessage="Listan är tom"
+                    collection={orderActions}
+                    columns={[
+                        { title: "Orderrad" },
+                        { title: "Åtgärd" },
+                        { title: "Antal", class: "uk-text-right" },
+                        { title: "Utförd", class: "uk-text-right" },
+                    ]}
+                    rowComponent={({ item }) => (
+                        <tr key={item.id}>
+                            <td>{item.id}</td>
+                            <td>{item.action_type}</td>
+                            <td className="uk-text-right">{item.value}</td>
+                            <td className="uk-text-right">
+                                {item.completed_at
+                                    ? dateTimeToStr(item.completed_at)
+                                    : "pending"}
+                            </td>
+                        </tr>
+                    )}
+                />
+            </div>
+        </div>
+    );
+};
 
 export default OrderShow;
