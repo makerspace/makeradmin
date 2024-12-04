e32c726497b8657b672b9bccc0efbf4925f4db1f
sofiareyesfranco <165427980+sofiareyesfranco@users.noreply.github.com>
2024-12-04 02:30:35 -0500

    changed class based to function based admin/src/Sales/ProductEdit.jsx:8

--- admin/src/Sales/ProductEdit.jsx
+++ admin/src/Sales/ProductEdit.jsx
@@ -1,37 +1,52 @@
-import React from "react";
-import { withRouter } from "react-router";
+import React, { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
 import Product from "../Models/Product";
 import ProductForm from "../Components/ProductForm";
 import { confirmModal } from "../message";
-import { browserHistory } from "../browser_history";
 
-class ProductEdit extends React.Component {
-    constructor(props) {
-        super(props);
-        const { id } = props.match.params;
-        this.product = Product.getWithRelated(id);
+const ProductEdit = () => {
+    const { id } = useParams(); // Use useParams to get the route parameter
+    const [product, setProduct] = useState(null);
+    const history = useHistory();
+
+    // Fetch the product on mount
+    useEffect(() => {
+        const fetchedProduct = Product.getWithRelated(id);
+        setProduct(fetchedProduct);
+    }, [id]);
+
+    const handleSave = () => {
+        if (product) {
+            product.save();
+        }
+    };
+
+    const handleDelete = () => {
+        if (product) {
+            confirmModal(product.deleteConfirmMessage())
+                .then(() => product.del())
+                .then(() => {
+                    history.push("/sales/product/");
+                })
+                .catch(() => null);
+        }
+    };
+
+    // Ensure the component has loaded the product before rendering
+    if (!product) {
+        return <div>Loading...</div>;
     }
 
-    render() {
-        return (
-            <div>
-                <h2>Redigera produkt</h2>
-                <ProductForm
-                    product={this.product}
-                    route={this.props.route}
-                    onSave={() => this.product.save()}
-                    onDelete={() => {
-                        return confirmModal(this.product.deleteConfirmMessage())
-                            .then(() => this.product.del())
-                            .then(() => {
-                                browserHistory.push("/sales/product/");
-                            })
-                            .catch(() => null);
-                    }}
-                />
-            </div>
-        );
-    }
-}
+    return (
+        <div>
+            <h2>Redigera produkt</h2>
+            <ProductForm
+                product={product}
+                onSave={handleSave}
+                onDelete={handleDelete}
+            />
+        </div>
+    );
+};
 
-export default withRouter(ProductEdit);
+export default ProductEdit;
