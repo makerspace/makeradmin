0c716bb4973f74d29196f8574fa3a137c82086a5
ilong4rennes <amanda.yichenlu@gmail.com>
2024-12-05 20:19:23 -0500

    refactored messages

--- admin/src/Messages/MessageShow.js
+++ admin/src/Messages/MessageShow.js
@@ -1,74 +1,66 @@
-import React from "react";
-import { withRouter } from "react-router";
-import Message from "../Models/Message";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import DateTimeShow from "../Components/DateTimeShow";
+import Message from "../Models/Message";
 
-class MessageShow extends React.Component {
-    constructor(props) {
-        super(props);
-        const { id } = props.match.params;
-        this.message = Message.get(id);
-        this.state = { message: {} };
-    }
+export default function MessageShow() {
+    const { id } = useParams();
+    const [message, setMessage] = useState({});
 
-    componentDidMount() {
-        this.unsubscribe = this.message.subscribe(() =>
-            this.setState({ message: this.message }),
-        );
-    }
+    useEffect(() => {
+        const messageInstance = Message.get(id);
+        const unsubscribe = messageInstance.subscribe(() => {
+            setMessage(messageInstance);
+        });
 
-    componentWillUnmount() {
-        this.unsubscribe();
-    }
+        return () => {
+            unsubscribe();
+        };
+    }, [id]);
 
-    render() {
-        const { message } = this.state;
-        return (
-            <div className="uk-margin-top">
-                <h2>Utskick</h2>
-                <div className="uk-panel uk-panel-box uk-margin-bottom">
-                    <table>
-                        <tbody>
-                            <tr>
-                                <th align="left">Created</th>
-                                <td>
-                                    <DateTimeShow date={message.created_at} />
-                                </td>
-                            </tr>
-                            <tr>
-                                <th align="left">Status</th>
-                                <td>{Message.statusText(message)}</td>
-                            </tr>
-                            <tr>
-                                <th align="left">Sent</th>
-                                <td>
-                                    <DateTimeShow date={message.sent_at} />
-                                </td>
-                            </tr>
-                            <tr>
-                                <th align="left">Recipient</th>
-                                <td>{message.recipient}</td>
-                            </tr>
-                            <tr>
-                                <th align="left">Template Used</th>
-                                <td>{message.template}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-
-                <div className="uk-panel uk-panel-box uk-margin-bottom">
-                    <h3
-                        className="uk-panel-title"
-                        dangerouslySetInnerHTML={{ __html: message.subject }}
-                    />
-                </div>
-                <div className="uk-panel uk-panel-box uk-margin-bottom">
-                    <div dangerouslySetInnerHTML={{ __html: message.body }} />
-                </div>
+    return (
+        <div className="uk-margin-top">
+            <h2>Utskick</h2>
+            <div className="uk-panel uk-panel-box uk-margin-bottom">
+                <table>
+                    <tbody>
+                        <tr>
+                            <th align="left">Created</th>
+                            <td>
+                                <DateTimeShow date={message.created_at} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th align="left">Status</th>
+                            <td>{Message.statusText(message)}</td>
+                        </tr>
+                        <tr>
+                            <th align="left">Sent</th>
+                            <td>
+                                <DateTimeShow date={message.sent_at} />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th align="left">Recipient</th>
+                            <td>{message.recipient}</td>
+                        </tr>
+                        <tr>
+                            <th align="left">Template Used</th>
+                            <td>{message.template}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-        );
-    }
-}
 
-export default withRouter(MessageShow);
+            <div className="uk-panel uk-panel-box uk-margin-bottom">
+                <h3
+                    className="uk-panel-title"
+                    dangerouslySetInnerHTML={{ __html: message.subject }}
+                />
+            </div>
+            <div className="uk-panel uk-panel-box uk-margin-bottom">
+                <div dangerouslySetInnerHTML={{ __html: message.body }} />
+            </div>
+        </div>
+    );
+}
