makerspace/makeradmin

commit 8d516fbc16862c243537114a58ef359a2471eb11

Remove pre-commit from mandatory setup

Makefile
@@ -46,7 +46,6 @@ init-npm:
 
 init-pip:
 	python3 -m pip install --upgrade -r requirements.txt
-	pre-commit install --install-hooks
 
 init: init-pip init-npm
 

commit 075b9bc0fa3e6afd029ddef877a70d99ff03cf1f

Correct the contribution guideline
Error introduced in 8d516fbc16862c243537114a58ef359a2471eb11

Makefile
@@ -47,6 +47,9 @@ init-npm:
 init-pip:
 	python3 -m pip install --upgrade -r requirements.txt
 
+init-pre-commit:
+	pre-commit install --install-hooks
+
 init: init-pip init-npm
 
 .env: