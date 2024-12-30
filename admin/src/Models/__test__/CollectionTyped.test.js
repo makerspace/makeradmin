792b5d0dd2e044a5c6fbde82b31b3d7a8e6163e4
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2024-12-30 15:56:22 +0100

    Test that subscribers get notified
    The @babel/pugin-transform-runtime is used for testing asynchronously,
    which simplifies the test implementation, in this case.

--- admin/src/Models/__test__/CollectionTyped.test.js
+++ admin/src/Models/__test__/CollectionTyped.test.js
@@ -3,6 +3,7 @@
  */
 
 /* eslint-env jest */
+import { get } from "../../gateway";
 import Collection from "../Collection";
 jest.mock("../../gateway");
 
@@ -60,5 +61,46 @@ describe("Fetch", () => {
             expand: "expand",
             search: "search",
         });
+    });
+});
+
+class FakeDatabase {
+    constructor() {
+        this.items = [];
+
+        get.mockImplementation(
+            (...v) => new Promise((resolve) => resolve(this.getResponse(...v))),
+        );
+    }
+
+    addItem(item) {
+        this.items.push(item);
+    }
+
+    getResponse({ url = "", params = {} }) {
+        const page_size = params.page_size || 10;
+        return {
+            data: this.items,
+            page: params.page || 1,
+            page_size: page_size,
+            last_page: Math.ceil(this.items.length / page_size),
+        };
+    }
+}
+
+describe("Subscription", () => {
+    test("each function gets notified", async () => {
+        const db = new FakeDatabase();
+        db.addItem("Group1");
+
+        const collection = new MockCollection();
+        collection.fetch();
+        const callback1 = jest.fn();
+
+        collection.subscribe(callback1);
+
+        await collection.fetch();
+
+        expect(callback1).toHaveBeenCalled();
     });
 });
