a2f4e55dc0afc0dd8c9170bf2f0fce2325ba20d1
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2025-01-06 08:14:48 +0100

    Convert SearchBox to Ui-kit 3

--- admin/src/Components/SearchBox.jsx
+++ admin/src/Components/SearchBox.jsx
@@ -3,25 +3,19 @@ import Icon from "./icons";
 
 const SearchBox = ({ value, handleChange }) => {
     return (
-        <div className="filterbox">
-            <div className="uk-grid">
-                <div className="uk-width-2-3">
-                    <form onSubmit={(e) => e.preventDefault()}>
-                        <div className="uk-form-icon">
-                            <Icon icon="search" />
-                            <input
-                                value={value}
-                                tabIndex="1"
-                                type="text"
-                                className="uk-form-width-large"
-                                placeholder="Skriv in ett sökord"
-                                onChange={(e) => handleChange(e.target.value)}
-                            />
-                        </div>
-                    </form>
-                </div>
+        <form className="filterbox" onSubmit={(e) => e.preventDefault()}>
+            <div className="uk-inline uk-width-1-1">
+                <Icon form icon="search" />
+                <input
+                    value={value}
+                    tabIndex="1"
+                    type="text"
+                    className="uk-input"
+                    placeholder="Skriv in ett sökord"
+                    onChange={(e) => handleChange(e.target.value)}
+                />
             </div>
-        </div>
+        </form>
     );
 };
 
