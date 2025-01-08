816c7134f017cbc6cdf78a6df85726323f1a94cc
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2025-01-08 20:08:19 +0100

    Fix bug when forwarding from root of page
    It crashed due to `DOMException: The operation is insecure` since the
    URL started with two slashes ("//").

---
+++ admin/src/Components/__test__/Routes.test.tsx
@@ -0,0 +1,21 @@
+import { url_join } from "../Routes";
+
+describe("url_join", () => {
+    it("should join paths correctly when part of the path ends with a slash", () => {
+        const paths = ["path/to/", "resource"];
+        const result = url_join(paths);
+        expect(result).toBe("path/to/resource");
+    });
+
+    it("should join paths correctly when no part of the path ends with a slash", () => {
+        const paths = ["path/to", "resource"];
+        const result = url_join(paths);
+        expect(result).toBe("path/to/resource");
+    });
+
+    it("should join paths correctly when all parts of the path end with a slash", () => {
+        const paths = ["path/to/", "resource/"];
+        const result = url_join(paths);
+        expect(result).toBe("path/to/resource");
+    });
+});
