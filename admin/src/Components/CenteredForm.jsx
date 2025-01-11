cd2ccac870cf231264b1bff5ff46f6a603606f22
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2025-01-11 09:33:29 +0100

    Add a nice border around the centered forms

--- admin/src/Components/CenteredForm.jsx
+++ admin/src/Components/CenteredForm.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const CenteredForm = ({ children }) => {
     return (
-        <div className="uk-flex uk-flex-center uk-flex-middle uk-text-center uk-height-1-1">
+        <div className="uk-flex uk-flex-center uk-flex-middle uk-text-center uk-height-1-1 centered-form">
             <div
                 className="uk-vertical-align-middle"
                 style={{ width: "500px" }}
