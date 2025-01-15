2fce86bf62bb5c18978f823ad199805b378814f7
Aron Granberg <aron.granberg@gmail.com>
2025-01-15 11:45:02 +0100

    Remove enzyme helper package as it's very old and not compatible with newer react. Remove one very simple test that depended on it.

--- admin/jestSetup.js
+++ admin/jestSetup.js
@@ -1,4 +1,5 @@
-import { configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+const config = {
+    verbose: true,
+};
 
-configure({ adapter: new Adapter() });
+module.exports = config;
