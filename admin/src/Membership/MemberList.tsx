af518b101ed9a8aa0439b591955bc154d10754b4
Aron Granberg <aron.granberg@gmail.com>
2025-08-26 13:17:48 +0200

    Improve searching for member numbers in member list.

--- admin/src/Membership/MemberList.tsx
+++ admin/src/Membership/MemberList.tsx
@@ -5,10 +5,10 @@ import Date from "../Components/DateShow";
 import Icon from "../Components/icons";
 import SearchBox from "../Components/SearchBox";
 import Collection from "../Models/Collection";
-import CollectionNavigation from "../Models/CollectionNavigation";
+import CollectionNavigation, { CollectionNavigationProps } from "../Models/CollectionNavigation";
 import Member from "../Models/Member";
 
-const Row = (props) => {
+const Row = (props: { item: Member, deleteItem: (item: Member) => void }) => {
     const { item, deleteItem } = props;
     return (
         <tr>
@@ -32,15 +32,46 @@ const Row = (props) => {
     );
 };
 
+class MemberCollection extends Collection<Member> {
+    override create_fetch_parameters() {
+        let params = super.create_fetch_parameters();
+
+        // If the search is a large integer, or a list of large integers,
+        // then search only for member numbers instead of doing a full search.
+        // This makes the search much more precise when searching for member numbers.
+        if (params.search) {
+            const intListMatch = params.search.match(/^\s*(\d\d\d\d+[\s,]*)+\s*$/);
+            if (intListMatch) {
+                const ids = params.search
+                    .split(/[\s,]+/)
+                    .map((v) => parseInt(v, 10))
+                    .filter((v) => !isNaN(v));
+                
+                return {
+                    ...params,
+                    regex: true,
+                    search: ids.join("|"),
+                    search_column: "member_number",
+                    sort_by: "member_number",
+                };
+            }
+        }
+
+        return params;
+    }
+}
+
 class MemberList extends CollectionNavigation {
-    constructor(props) {
+    collection: Collection<Member>;
+
+    constructor(props: CollectionNavigationProps) {
         super(props);
         const { search, page } = this.state;
 
-        this.collection = new Collection({ type: Member, search, page });
+        this.collection = new MemberCollection({ type: Member, search, page });
     }
 
-    render() {
+    override render() {
         const columns = [
             { title: "#", sort: "member_id" },
             { title: "Förnamn", sort: "firstname" },
