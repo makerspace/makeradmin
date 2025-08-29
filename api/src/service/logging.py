524e2bca91821f814dc90e284df84e85c70722ad
Aron Granberg <aron.granberg@gmail.com>
2025-08-29 21:48:20 +0200

    Use a custom logger with colors.

--- api/src/service/logging.py
+++ api/src/service/logging.py
@@ -1,11 +1,37 @@
+import logging
 import sys
 from logging import INFO, basicConfig, getLogger
 
+
+class CustomFormatter(logging.Formatter):
+    grey = "\x1b[38;20m"
+    yellow = "\x1b[33;20m"
+    red = "\x1b[31;20m"
+    bold_red = "\x1b[31;1m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s %(name)s:T%(process)d (%(filename)s:%(lineno)d) %(levelname)s: %(message)s"
+
+    FORMATS = {
+        logging.DEBUG: grey + format + reset,
+        logging.INFO: grey + format + reset,
+        logging.WARNING: yellow + format + reset,
+        logging.ERROR: red + format + reset,
+        logging.CRITICAL: bold_red + format + reset,
+    }
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt)
+        return formatter.format(record)
+
+
 basicConfig(
-    format="%(asctime)s %(levelname)s [%(process)d/%(threadName)s %(pathname)s:%(lineno)d]: %(message)s",
+    format="%(asctime)s %(levelname)s:%(name)s:T%(process)d: %(message)s",
     stream=sys.stderr,
     level=INFO,
 )
+for handler in logging.root.handlers:
+    handler.setFormatter(CustomFormatter())
 
 logger = getLogger("makeradmin")
 
