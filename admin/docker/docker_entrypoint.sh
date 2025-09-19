47abfcdf0f7d5979c47a0a46b8e739d3ebd34a5e
Aron Granberg <aron.granberg@gmail.com>
2025-09-19 16:39:37 +0200

    Fix dev mode not working when running behind a reverse-proxy. Updated admin dev container base.

--- admin/docker/docker_entrypoint.sh
+++ admin/docker/docker_entrypoint.sh
@@ -2,16 +2,17 @@
 
 chown -R nginx:nginx /var/www/html
 
-BACKEND_HOST=${HOST_BACKEND#*://}
-BACKEND_PROTOCOL=${HOST_BACKEND%${BACKEND_HOST}}
-
 echo \
 "var config = {
-	apiBasePath: \"${BACKEND_PROTOCOL:-https://}${BACKEND_HOST}\",
+	apiBasePath: \"${HOST_BACKEND}\",
 	apiVersion: \"1.0\",
 	pagination: {
 		pageSize: 25,
 	},
 }" > /var/www/html/js/config.js
 
-nginx -g 'daemon off;'
+if [ "$DEV_RUN" = "true" ]; then
+	exec npm run --silent dev
+else
+	exec nginx -g 'daemon off;'
+fi
